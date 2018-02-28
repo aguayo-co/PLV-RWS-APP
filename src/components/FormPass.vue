2d8efa534e55da4812d37086892e5c436cfeedcb
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-02-28 16:08:51 -0500

    [Front-end] Notificaciones Recuperar contraseña: Notificación de éxito recuperar contraseña

--- src/components/FormPass.vue
+++ src/components/FormPass.vue
@@ -16,6 +16,8 @@
     .form__row.form__row_away
       button.btn.btn_solid.btn_block(
         @click.prevent='Login()') Enviar Correo
+
+    p.form__info.i-alert-info Recuerda revisar también la carpeta de <strong>correo no deseado.</strong>
 </template>
 
 <script>
