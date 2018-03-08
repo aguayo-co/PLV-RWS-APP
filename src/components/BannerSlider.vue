cdac2cf163066af57690134c8b65a0567e0e5da7
Yehimy Rincón M <yehimy.rincon@aguayopublicidad.com>
2018-03-08 10:21:31 -0500

    [Frontend - Prilov] :  La salvación de los archivos

--- src/components/BannerSlider.vue
+++ src/components/BannerSlider.vue
@@ -1,9 +1,45 @@
 <template lang="pug">
 .banner-slider
+  swiper.banner-slider__container(:options='swiperOption')
+    swiper-slide.banner-slider__slide(v-for='slide in slides')
+      .banner-slider__slot
+        h3.bg_white.banner-slider__title Te regalamos en tu primera compra
+        a.banner-slider__btn.btn Promo Code:
+          span.banner-slider__code Primera Compra
+        span.banner-slider__copy **Oferta válida hasta el 31 de enero de 2018
+      img.banner-slider__img(:src='slide.path')
+    swiper-pagination
 </template>
 
 <script>
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+Vue.use(VueAwesomeSwiper)
+
 export default {
-  name: 'BannerSlider'
+  name: 'BannerSlider',
+  components: {
+    VueAwesomeSwiper
+  },
+  data () {
+    return {
+      slides: [
+        { path: '/static/img/demo/banner-slider1.png'},
+        { path: '/static/img/demo/banner-slider2.png'},
+        { path: '/static/img/demo/banner-slider3.png'}
+      ],
+      swiperOption: {
+        sliderPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 0,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true
+        }
+      }
+    }
+  }
 }
 </script>
