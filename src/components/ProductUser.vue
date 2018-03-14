74ef62456b66fdc47cdf009674897233c144ee61
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-03-14 18:49:34 -0500

    [ Frontend-2: [dev-product-detail] Prilov ]:  Bloque sobre la vendedora

--- src/components/ProductUser.vue
+++ src/components/ProductUser.vue
@@ -5,35 +5,39 @@
       header.single__header
         h2.subhead Sobre la vendedora
       //- data User
-      .user-data.user-data__product(
+      .user-data.user-card(
         v-for="data in user")
-        .user-header
-          .user-header__item
-            .user-data__avatar
-              //- clic en foto de perfil de usuario
-                Se despliega un input (file) donde el usuario
-                puede subir y editar su foto de perfil.
-              img.user-data__img(
-                :src="data.picture",
-                :alt="'Perfil' + ' ' + data.first_name")
-            p.user-data__caption-i.i-star-on Prilovestar
-          .user-header__item
-            .user-header-edit(
-              :class="{'user-header-edit_active' :editName == true}")
-              a.user-header-edit__item(
-                @click.prevent="EditName()",
-                href="#",
-                title="Editar Nombre y apellido")
-                h3.user-data__title
-                  | {{ data.first_name }} {{ data.last_name }}
-            .user-data__notify
-              ul.user-data__list
-                li.user-data__value.i-like 20
-                li.user-data__value.i-like.i_flip 0
-                li.user-data__value.i-less-circle 0
-              ul.user-data__list
-                li.user-data__track {{ data.followers_count }} Seguidores
-                li.user-data__track {{ data.following_count }} Siguiendo
+        .user-card__block
+          .user-header
+            .user-header__item
+              .user-data__avatar
+                //- clic en foto de perfil de usuario
+                  Se despliega un input (file) donde el usuario
+                  puede subir y editar su foto de perfil.
+                img.user-data__img(
+                  :src="data.picture",
+                  :alt="'Perfil' + ' ' + data.first_name")
+              p.user-data__caption-i.i-star-on Prilovestar
+            .user-header__item
+              .user-header-edit(
+                :class="{'user-header-edit_active' :editName == true}")
+                a.user-header-edit__item(
+                  href="#",
+                  title="Editar Nombre y apellido")
+                  h3.user-data__title
+                    | {{ data.first_name }} {{ data.last_name }}
+              .user-data__notify
+                ul.user-data__list
+                  li.user-data__value.i-like 20
+                  li.user-data__value.i-like.i_flip 0
+                  li.user-data__value.i-less-circle 0
+                ul.user-data__list
+                  li.user-data__track {{ data.followers_count }} Seguidores
+                  li.user-data__track {{ data.following_count }} Siguiendo
+        .user-card__block
+          .user-bullet
+            p.user-bullet__txt Compradora compulsiva en rehabilitación, todo lo que ves en mi perfil está disponible y en excelente estado. Todas las ventas se hacen por Prilov.
+            a(href='/' title='Enviar mensaje privado').user-bullet__link.i-send Enviar mensaje privado
 </template>
 
 <script>
