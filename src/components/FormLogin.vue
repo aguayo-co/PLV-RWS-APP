be2aef729ecfd4a451e4f463fe19750f62c8f9e0
Juan David Saab <juan.saab@aguayo.co>
2018-03-13 22:18:05 -0500

    [Vue] - Se añade validación al formulario de Login. [Store] - Se conecta el formulario con el nuevo store User

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -12,32 +12,42 @@ transition(name='modal-fade')
         form.form(
           v-on:submit='',
           action='#',
+          submit.prevent='validateBeforeSubmit',
           method='post'
         )
-          .form__row
+          .form__row(
+            v-bind:class='{ "is-danger": errorTexts.email }'
+          )
             label.form__label(
-              for='userEmail') Correo
+              for='email') Correo
+            span.help(
+              v-if="errorTexts.email"
+            ) {{ errorTexts.email }}
             input.form__control(
-              v-model='userEmail',
-              id='userEmail',
+              v-model='email',
+              id='email',
               type='email')
-          .form__row
+          .form__row(
+            v-bind:class='{ "is-danger": errorTexts.password }'
+          )
             label.form__label(
-              for='userPsw'
+              for='password'
             ) Contraseña
+            span.help(
+              v-if="errorTexts.password"
+            ) {{ errorTexts.password }}
             input.form__control(
-              v-model='userPsw',
-              id='userPsw',
+              v-model='password',
+              id='password',
               type='password')
             p.form__note.form__note_right
               |¿Olvidaste tu contraseña?
               | <a class='link_underline' @click='close' href='#/password' title='Ir a recuperar contraseña'>Recuperar contraseña.</a>
           .form__row.form__row_away
             button.btn.btn_solid.btn_block(
-              @click.prevent='login()') Iniciar sesión
+              @click.prevent='validateBeforeSubmit') Iniciar sesión
         .break
           span.break__txt O
-        //- falta cerrar la modal
         router-link.btn.btn_block(
           to='SignUp',
           title='Ir a Registro') Regístrate
@@ -49,33 +59,41 @@ export default {
   name: 'FormLogin',
   data () {
     return {
-      userEmail: '',
-      userPsw: ''
+      email: '',
+      password: '',
+      errorTexts: {}
     }
   },
   methods: {
+    validateBeforeSubmit: function () {
+      this.errorTexts = {}
+
+      if (!this.email) {
+        this.errorTexts.email = 'Debes ingresar tu email'
+      } else {
+        if (!/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/.test(this.email)) {
+          this.errorTexts.email = 'El email que ingresaste no parece válido.'
+        }
+      }
+
+      if (!this.password) this.errorTexts.password = 'Debes ingresar una contraseña'
+
+      if (Object.keys(this.errorTexts).length === 0) {
+        this.login()
+      }
+    },
     login: function () {
       axios.post('https://prilov.aguayo.co/api/users/login', {
-        email: this.userEmail,
-        password: this.userPsw
+        email: this.email,
+        password: this.password
       })
         .then(response => {
           console.log(response.data)
-          // this.$store.tokenLocalStorage.getItem('token')
-          // this.$store.dispatch('signUp/actionSetToken', response.data.api_token)
-          this.$store.dispatch('UserModule/actionSetToken', response.data.api_token)
-          this.$store.dispatch('UserModule/actionSetUserName', response.data.first_name)
-          this.$store.dispatch('UserModule/actionSetAuth')
-          // this.$store.dispatch('actionSetAuth')
-          // this.$store.getters.getAuth()
-          console.log(this.$store.getters['UserModule/getToken'])
-          localStorage.setItem('token', this.$store.getters['UserModule/getToken'])
-          localStorage.setItem('userName', this.$store.getters['UserModule/getUserName'])
-          console.log('TOKEN EN LS')
-          localStorage.getItem('token')
+          localStorage.setItem('token', response.data.api_token)
+          localStorage.setItem('userId', response.data.id)
+
+          this.$store.dispatch('user/setUser', response.data)
           this.close()
-          // console.log(this.userToken)
-          // this.$store.tokenLocalStorage.setItem('userAuth', {token: response.data.api_token})
         })
         .catch(e => {
           console.log('ERROR : ' + e)
