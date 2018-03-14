dba5a96b724f1fce0b438f7e2c4db3afd4d7b042
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-03-14 11:57:43 -0500

    [ Frontend-2: [dev-product-detail] Prilov ]:  Creación del bloque header...

--- src/components/slider-producto.vue
+++ src/components/slider-producto.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
 .layout-inner
-  p soy un slider
+  section.section_product
+    header.section__head.section__head_big
+      h2.title_head Otras prendas que <span class="txt_brand">te pueden interesar</span>
+      h3.title_subhead Elegimos otras prendas que creemos te pueden interesar
+
 </template>
 
 <script>
