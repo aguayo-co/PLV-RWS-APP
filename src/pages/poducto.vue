bf249b40d82674f239ba190b8b67c1a97d5d186a
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-03-14 09:05:57 -0500

    [Front-end] Instancia producto

---
+++ src/pages/poducto.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.layout-page
+  ProductoDetalle
+</template>
+
+<script>
+import ProductoDetalle from '@/components/Poducto-detalle'
+export default {
+  name: 'ProductoPage',
+  components: {
+    ProductoDetalle
+  }
+}
+</script>
