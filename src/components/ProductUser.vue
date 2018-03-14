5c16fd3ba9ff1cf047ff6118daed733eee996e03
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-03-14 17:39:34 -0500

    [ Frontend-2: [dev-product-detail] Prilov ]:  Bloque de user dentro de la página de producto

--- src/components/ProductUser.vue
+++ src/components/ProductUser.vue
@@ -5,87 +5,35 @@
       header.single__header
         h2.subhead Sobre la vendedora
       //- data User
-      .user-data(
+      .user-data.user-data__product(
         v-for="data in user")
-        //-TO-DO if AUTH datos dinámicos USER
-        form.form(
-          id="form-user-data"
-          v-on:submit='',
-          action='#',
-          method='post')
-          .user-header
-            .user-header__item
-
-              .user-data__avatar
-                //- clic en foto de perfil de usuario
-                  Se despliega un input (file) donde el usuario
-                  puede subir y editar su foto de perfil.
-                img.user-data__img(
-                  :src="data.picture",
-                  :alt="'Perfil' + ' ' + data.first_name")
-            .user-header__item
-              .user-header-edit(
-                :class="{'user-header-edit_active' :editName == true}")
-                a.user-header-edit__item(
-                  @click.prevent="EditName()",
-                  href="#",
-                  title="Editar Nombre y apellido")
-                  h3.user-data__title
-                    | {{ data.first_name }} {{ data.last_name }}
-                  span.user-edit__actions
-                    span.btn_edit.i-edit-line <small class="hide"> Editar </small>
-                //-TO-do animate slide
-                transition(slide-toggle)
-                  .user-header-edit__grid(
-                    v-show="editName == true")
-                    .form__row
-                      label.form__label(
-                        for='nombre') Editar Nombre
-                      input.form__edit(
-                        v-model='data.first_name',
-                        id='username'
-                      )
-                    .form__row
-                      label.form__label(
-                        for='apellidos') Editar Apellidos
-                      input.form__edit(
-                        v-model='data.last_name',
-                        id='userLast',
-                        type='text'
-                      )
-
-              //-Enlaces Modal
-              //- ul.user-data-nav
-              //-   li.user-data-nav__item Cambiar contraseña   |
-              //-   li.user-data-nav__item Eliminar cuenta
-              //-Notificaciones
-              .user-data__notify
-                ul.user-data__list
-                  li.user-data__value.i-like 20
-                  li.user-data__value.i-like.i_flip 0
-                  li.user-data__value.i-less-circle 0
-                ul.user-data__list
-                  li.user-data__track {{ data.followers_count }} Seguidores
-                  li.user-data__track {{ data.following_count }} Siguiendo
-
-          //-editar About perfil
-          .user-header-edit
-            .user-header-edit__item.edit__item_top
-              .form__row_top
-                p.user-data__txt(
-                  v-if="editAbout == false",
-                  @click.prevent="EditAbout()") {{ data.about }}
-                textarea.form__edit_txt(
-                  v-model="data.about",
-                  name="about",
-                  maxlength="340",
-                  v-if="editAbout == true",
-                  form="form-user-data")
-              a.user-edit__actions(
-                @click.prevent="EditAbout()",
-                href="#"
-                title="Editar perfil")
-                span.btn_edit.i-edit-line <small class="hide"> Editar </small>
+        .user-header
+          .user-header__item
+            .user-data__avatar
+              //- clic en foto de perfil de usuario
+                Se despliega un input (file) donde el usuario
+                puede subir y editar su foto de perfil.
+              img.user-data__img(
+                :src="data.picture",
+                :alt="'Perfil' + ' ' + data.first_name")
+            p.user-data__caption-i.i-star-on Prilovestar
+          .user-header__item
+            .user-header-edit(
+              :class="{'user-header-edit_active' :editName == true}")
+              a.user-header-edit__item(
+                @click.prevent="EditName()",
+                href="#",
+                title="Editar Nombre y apellido")
+                h3.user-data__title
+                  | {{ data.first_name }} {{ data.last_name }}
+            .user-data__notify
+              ul.user-data__list
+                li.user-data__value.i-like 20
+                li.user-data__value.i-like.i_flip 0
+                li.user-data__value.i-less-circle 0
+              ul.user-data__list
+                li.user-data__track {{ data.followers_count }} Seguidores
+                li.user-data__track {{ data.following_count }} Siguiendo
 </template>
 
 <script>
