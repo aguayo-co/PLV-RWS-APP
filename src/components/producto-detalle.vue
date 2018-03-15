c3ec863f92e260f84c28c22646226bf7053b4a97
Geraldine Díaz <geraldine.diaz@aguayopublicidad.com>
2018-03-14 22:22:24 -0500

    Front-end [Prilov]: consumo por metodo get para detalle de producto

--- src/components/producto-detalle.vue
+++ src/components/producto-detalle.vue
@@ -26,16 +26,16 @@
           swiper-slide.slide-4
             img(src="/static/img/demo/producto/mochila-dorada-004.jpg", alt="")
     .detail-content
-      p.detail-content__title Sandalias de Cuero y Tachas Saville Row
+      p.detail-content__title {{ product.title }}
       span.detail-content__label Prenda usada
-      h3.detail-content__brand Straadivaruois & Zara Home Juntos
-      p.detail-content__size Talla: S | Color: Blanco
-      p.detail-content__price_old $70.000
-      p.detail-content__price $40.000
+      h3.detail-content__brand {{ product.brand.name }}
+      p.detail-content__size Talla: {{ product.dimensions }} | Color: {{ product.colors["0"].name }}
+      p.detail-content__price_old {{ product.original_price }}
+      p.detail-content__price {{ product.price }}
       a.btn.btn_solid Comprar
       a.btn.i-heart Agregar a Favoritos
       p.detail-content__subtitle Descripción
-      p.detail-content__txt Sweater peludo manga larga talla S. Es COOOOMODISIMO, el típico que usas para TODO. Calientito. Color crudo. Lo compre por ASOS.
+      p.detail-content__txt {{ product.description }}
       p.detail-content__subtitle Despacho
       p.detail-content__txt.i-ok Envíos a todo Chile
         span.detail-content__txt.detail-content__txt_small Mediante Chile-express o Correos de Chile
@@ -45,7 +45,7 @@
 <script>
 import 'swiper/dist/css/swiper.min.css'
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
-
+import axios from 'axios'
 export default {
   name: 'ProductoDetalle',
   components: {
@@ -54,19 +54,10 @@ export default {
   },
   data () {
     return {
-      // swiperOptionTop: {
-      //   spaceBetween: 0,
-      //   loop: true,
-      //   loopedSlides: 4,
-      //   pagination: {
-      //     el: '.swiper-pagination',
-      //     dynamicBullets: true
-      //   }
-      // },
       swiperOptionTop: {
         spaceBetween: 10,
         loop: true,
-        loopedSlides: 5, //looped slides should be the same
+        loopedSlides: 5,
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev'
@@ -83,43 +74,31 @@ export default {
         autoHeight: true,
         touchRatio: 1,
         loop: true,
-        loopedSlides: 5, //looped slides should be the same
-        slideToClickedSlide: true,
-      }
+        loopedSlides: 5,
+        slideToClickedSlide: true
+      },
+      product: {}
     }
   },
-    mounted() {
-      this.$nextTick(() => {
-        const swiperTop = this.$refs.swiperTop.swiper
-        const swiperThumbs = this.$refs.swiperThumbs.swiper
-        swiperTop.controller.control = swiperThumbs
-        swiperThumbs.controller.control = swiperTop
+  mounted () {
+    this.$nextTick(() => {
+      const swiperTop = this.$refs.swiperTop.swiper
+      const swiperThumbs = this.$refs.swiperThumbs.swiper
+      swiperTop.controller.control = swiperThumbs
+      swiperThumbs.controller.control = swiperTop
+    })
+  },
+  async created () {
+    await axios.get('https://prilov.aguayo.co/api/products/3', {
+    })
+      .then(response => {
+        console.log(response)
+        this.product = response.data
+        // this.nameFooter = response.data.items[3].name
       })
-    }
+      .catch(e => {
+        console.log('ERROR : ' + e)
+      })
+  }
 }
 </script>
-<style scoped>
-  .swiper-container {
-    padding-top: 50px;
-  }
-  .gallery-main {
-    width: 100%;
-  }
-  .gallery-thumb {
-    box-sizing: border-box;
-    padding: 10px 0;
-  }
-  .gallery-thumb .swiper-slide {
-    width: 25%;
-    height: 100%;
-    opacity: 0.4;
-  }
-  .gallery-thumb .swiper-slide-active {
-    opacity: 1;
-  }
-
-  .gallery-thumb .swiper-slide img {
-    width: 100%;
-    max-width: 85px;
-  }
-</style>
