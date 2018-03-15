d59d9e70abf1c2b35fc79f9fad3f4b6aeb2f0353
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-03-14 19:04:34 -0500

    [Front-end] Modulo upload file detalles títulos

---
+++ src/components/PublicarVenta-step1.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+    //- :show-remove-button="false"
+    .layout-inner
+      .upfile
+        .upfile__main.i-plus
+          h4.upfile__title Foto Principal
+          .upfile__item
+            .upfile__label
+              .upfile__text.i-upload(
+                v-if="mqDesk") Arrastra una foto o
+              .upfile__btn Sube una imagen
+            croppa(
+              :width="400",
+              :height="600",
+              :quality="2",
+              placeholder="",
+              :prevent-white-space="true")
+        .upfile__group
+          h4.upfile__title Fotos Secundarias (opcionales)
+          .upfile__grid
+            .upfile__item
+              .upfile__label
+                .upfile__text.i-upload(
+                  v-if="mqDesk") Arrastra una foto o
+                .upfile__btn Sube una imagen
+              croppa(
+                :width="400",
+                :height="600",
+                :quality="2",
+                placeholder="",
+                :prevent-white-space="true")
+            .upfile__item
+              .upfile__label
+                .upfile__text.i-upload(
+                  v-if="mqTablet") Arrastra una foto o
+                .upfile__btn Sube una imagen
+              croppa(
+                :width="400",
+                :height="600",
+                :quality="2",
+                placeholder="",
+                :prevent-white-space="true")
+            .upfile__item(
+              v-if="mqDesk")
+              .upfile__label
+                .upfile__text.i-upload(
+                  v-if="mqTablet") Arrastra una foto o
+                .upfile__btn Sube una imagen
+              croppa(
+                :width="400",
+                :height="600",
+                :quality="2",
+                placeholder="",
+                :prevent-white-space="true")
+</template>
+
+<script>
+import Vue from 'vue'
+import Croppa from 'vue-croppa'
+Vue.component('croppa', Croppa.component)
+
+export default {
+  name: 'PublicarVentaStep2'
+  // data () {
+  //   return {
+  //     uploadMain: {}
+  //   }
+  // }
+}
+</script>
--- src/pages/publicar-venta.vue
+++ src/pages/publicar-venta.vue
@@ -18,74 +18,20 @@
     submit.prevent='',
     method='post')
     //-Formulario step 1 formlario Foto
-    //- :show-remove-button="false"
-    .layout-inner
-      .upfile
-        .upfile__main.i-plus
-          h4.upfile__title Foto Principal
-          .upfile__item
-            .upfile__label
-              .upfile__text.i-upload(
-                v-if="mqDesk") Arrastra una foto o
-              .upfile__btn Sube una imagen
-            croppa(
-              :width="400",
-              :height="600",
-              :quality="2",
-              placeholder="",
-              :prevent-white-space="true")
-        .upfile__group
-          h4.upfile__title Fotos Secundarias (opcionales)
-          .upfile__grid
-            .upfile__item
-              .upfile__label
-                .upfile__text.i-upload(
-                  v-if="mqDesk") Arrastra una foto o
-                .upfile__btn Sube una imagen
-              croppa(
-                :width="400",
-                :height="600",
-                :quality="2",
-                placeholder="",
-                :prevent-white-space="true")
-            .upfile__item
-              .upfile__label
-                .upfile__text.i-upload(
-                  v-if="mqTablet") Arrastra una foto o
-                .upfile__btn Sube una imagen
-              croppa(
-                :width="400",
-                :height="600",
-                :quality="2",
-                placeholder="",
-                :prevent-white-space="true")
-            .upfile__item(
-              v-if="mqDesk")
-              .upfile__label
-                .upfile__text.i-upload(
-                  v-if="mqTablet") Arrastra una foto o
-                .upfile__btn Sube una imagen
-              croppa(
-                :width="400",
-                :height="600",
-                :quality="2",
-                placeholder="",
-                :prevent-white-space="true")
-
+    PublicarVentaStep1
     //-Formulario step Formulario producto
     PublicarVentaStep2
 
 </template>
 
 <script>
+import PublicarVentaStep1 from '@/components/PublicarVenta-step1'
 import PublicarVentaStep2 from '@/components/PublicarVenta-step2'
-import Vue from 'vue'
-import Croppa from 'vue-croppa'
-Vue.component('croppa', Croppa.component)
 
 export default {
   name: 'PublicarVenta',
   components: {
+    PublicarVentaStep1,
     PublicarVentaStep2
   }
   // data () {
