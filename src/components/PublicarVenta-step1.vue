e96b9007131058f31fa438f323ea644a92725c48
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-03-14 21:03:03 -0500

    [Front-end] Cambio header página, se elimina barra de  steps y se integra al band del top el upload de la imágenes de formulario

--- src/components/PublicarVenta-step1.vue
+++ src/components/PublicarVenta-step1.vue
@@ -1,57 +1,56 @@
 <template lang="pug">
-    //- :show-remove-button="false"
-    .layout-inner
-      .upfile
-        .upfile__main.i-plus
-          h4.upfile__title Foto Principal
-          .upfile__item
-            .upfile__label
-              .upfile__text.i-upload(
-                v-if="mqDesk") Arrastra una foto o
-              .upfile__btn Sube una imagen
-            croppa(
-              :width="400",
-              :height="600",
-              :quality="2",
-              placeholder="",
-              :prevent-white-space="true")
-        .upfile__group
-          h4.upfile__title Fotos Secundarias (opcionales)
-          .upfile__grid
-            .upfile__item
-              .upfile__label
-                .upfile__text.i-upload(
-                  v-if="mqDesk") Arrastra una foto o
-                .upfile__btn Sube una imagen
-              croppa(
-                :width="400",
-                :height="600",
-                :quality="2",
-                placeholder="",
-                :prevent-white-space="true")
-            .upfile__item
-              .upfile__label
-                .upfile__text.i-upload(
-                  v-if="mqTablet") Arrastra una foto o
-                .upfile__btn Sube una imagen
-              croppa(
-                :width="400",
-                :height="600",
-                :quality="2",
-                placeholder="",
-                :prevent-white-space="true")
-            .upfile__item(
-              v-if="mqDesk")
-              .upfile__label
-                .upfile__text.i-upload(
-                  v-if="mqTablet") Arrastra una foto o
-                .upfile__btn Sube una imagen
-              croppa(
-                :width="400",
-                :height="600",
-                :quality="2",
-                placeholder="",
-                :prevent-white-space="true")
+  //- :show-remove-button="false"
+  .upfile
+    .upfile__main.i-plus
+      h4.upfile__title Foto Principal
+      .upfile__item
+        .upfile__label
+          .upfile__text.i-upload(
+            v-if="mqDesk") Arrastra una foto o
+          .upfile__btn Sube una imagen
+        croppa(
+          :width="400",
+          :height="600",
+          :quality="2",
+          placeholder="",
+          :prevent-white-space="true")
+    .upfile__group
+      h4.upfile__title Fotos Secundarias (opcionales)
+      .upfile__grid
+        .upfile__item
+          .upfile__label
+            .upfile__text.i-upload(
+              v-if="mqDesk") Arrastra una foto o
+            .upfile__btn Sube una imagen
+          croppa(
+            :width="400",
+            :height="600",
+            :quality="2",
+            placeholder="",
+            :prevent-white-space="true")
+        .upfile__item
+          .upfile__label
+            .upfile__text.i-upload(
+              v-if="mqTablet") Arrastra una foto o
+            .upfile__btn Sube una imagen
+          croppa(
+            :width="400",
+            :height="600",
+            :quality="2",
+            placeholder="",
+            :prevent-white-space="true")
+        .upfile__item(
+          v-if="mqDesk")
+          .upfile__label
+            .upfile__text.i-upload(
+              v-if="mqTablet") Arrastra una foto o
+            .upfile__btn Sube una imagen
+          croppa(
+            :width="400",
+            :height="600",
+            :quality="2",
+            placeholder="",
+            :prevent-white-space="true")
 </template>
 
 <script>
--- src/pages/publicar-venta.vue
+++ src/pages/publicar-venta.vue
@@ -1,27 +1,19 @@
 <template lang="pug">
 .layout-page
-  .layout-band
-    .layout-inner
-      header.page__head.steps__header
-        h1.title Publicar una venta
-        h2.title_subhead ¿No lo usas?, ¡Vendelo!
-        ul.steps__list
-          li.steps__item.steps__item_current
-            span.steps__bullet.i-ok 1
-            span.steps__label Producto
-          li.steps__item
-            span.steps__bullet.i-ok  2
-            span.steps__label Descripción
   form.form.form_big(
     id='form-publicar',
     action='#',
     submit.prevent='',
     method='post')
-    //-Formulario step 1 formlario Foto
-    PublicarVentaStep1
+    .layout-band
+      .layout-inner
+        header.page__head.steps__header
+          h1.title__main Publicar una venta
+          h2.title_subhead ¿No lo usas?, ¡Vendelo!
+        //-Formulario step 1 formlario Foto
+        PublicarVentaStep1
     //-Formulario step Formulario producto
     PublicarVentaStep2
-
 </template>
 
 <script>
