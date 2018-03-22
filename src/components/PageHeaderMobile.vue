6d075ef720831f6b0974c2e9737f7566e78254c8
Geraldine Díaz <geraldine.diaz@aguayopublicidad.com>
2018-03-22 07:14:42 -0500

    Front-end [MOVii]: funcionalidad skicky fail :'(

--- src/components/PageHeaderMobile.vue
+++ src/components/PageHeaderMobile.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 //- HEADER PAGE
-.page-header(v-sticky="{ zIndex: 50, stickyTop: 20 }")
+.page-header
   .layout-inner
     header.header__bar
       span.menu-ico(
@@ -29,7 +29,6 @@
 <script>
 import PageHeaderBrand from '@/components/PageHeader-brand'
 import PageHeaderMenuMobile from '@/components/PageHeader-menuMobile'
-import VueSticky from 'vue-sticky'
 
 export default {
   name: 'PageHeaderMobile',
@@ -37,52 +36,26 @@ export default {
     PageHeaderBrand,
     PageHeaderMenuMobile
   },
-  directives: {
-    'sticky': VueSticky
-  },
   data () {
     return {
       menuOpen: false,
-      stickyOptions: {
-        marginTop: 20,
-        forName: 0,
-        className: 'stuck'
+      header: {
+        height: 0,
+        window: 0,
+        windowScrollTop: 0
       }
     }
+  },
+  created () {
+    window.addEventListener('scroll', this.handleScroll)
   },
   methods: {
     MenuOpen: function () {
       this.menuOpen = !this.menuOpen
     }
+    // handleScroll: _.throttle(function () {
+    //   this.header.windowScrollTop = window.pageYOffset || document.documentElement.scrollTop
+    // }, 100)
   }
 }
 </script>
-<style>
-body {
-  font-family: sans-serif;
-}
-
-.container {
-  height: 2000px;
-}
-
-.box {
-  padding: 10px;
-  box-sizing: border-box;
-  border: 2px solid #3d3d3d;
-  width: 100%;
-  text-align: center;
-  line-height: 75px;
-  font-size: 20px;
-  background: #f3f3f3;
-  margin-bottom: 10px;
-}
-
-.stuck {
-  background: blue !important;
-}
-
-.stuck-2 {
-  background: red !important;
-}
-</style>
