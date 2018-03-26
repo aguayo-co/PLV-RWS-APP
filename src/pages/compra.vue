742919e178d9c8f2dc9db0cf81f53af2e2c56fff
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-03-26 08:03:37 -0500

    [Prilov] [Maquetado] - compra contenidos maquetación pagos y varios items

--- src/pages/compra.vue
+++ src/pages/compra.vue
@@ -25,123 +25,7 @@
           //-Compra Listado de productos: 1a Cliente Registrado, logueado
           CompraCarro
           //-Compra 1b Cliente Registrado, logueado
-          section.list_step
-            h3.subhead Elige una opción de despacho
-            //-checkbox tipo envíos
-            .boxcheck
-              .boxcheck__grid.boxcheck__grid_tip
-                //-item 1
-                .boxcheck__item
-                  input.form__input-radio(
-                    id="estandar",
-                    type="radio",
-                    name="envio",
-                    value="estandar",
-                    checked="checked")
-                  label.form__label.form__label_radio(
-                    for="estandar")
-                    span.boxcheck__label
-                      span Envío estándar (Chilexpress) <small class="boxcheck__disclaimer">($3000)</small>
-                  //-nota
-                  .boxcheck__tip
-                    p Al seleccionar esta opción de despacho, las entregas son según su disponibilidad y efectuadas de Lunes a Sábado.
-                    p Tiempo de entrega entre 1 y 5 días hábiles
-                //-end item 1
-                //-item 2
-                .boxcheck__item
-                  input.form__input-radio(
-                    id="ultra",
-                    type="radio",
-                    name="envio",
-                    value="ultra",
-                    checked="checked")
-                  label.form__label.form__label_radio(
-                    for="ultra")
-                    span.boxcheck__label
-                      span Envío ultra rápido (Chilexpress) <small class="boxcheck__disclaimer">($5000)</small>
-                //-end item 2
-                //-item 3
-                .boxcheck__item
-                  input.form__input-radio(
-                    id="saleswoman",
-                    type="radio",
-                    name="envio",
-                    value="saleswoman",
-                    checked="checked")
-                  label.form__label.form__label_radio(
-                    for="saleswoman")
-                    span.boxcheck__label
-                      span Acuerdo envío con vendedora <small class="boxcheck__disclaimer">($3000)</small>
-                //-end item 3
-            //-end checkbox tipo envíos
-
-            //- direcciones| Correo | tel
-                Este componente se toma de User/data
-                se integran las funcionalidades desarrolladas
-                por Federico.
-                archivo /components/CompraFormUserData.vue
-            UserData
-            //-End direcciones| Correo | tel
-
-            //-créditos
-            h3.subhead ¿Quieres usar tus créditos en esta compra?
-            .pay-off
-              .pay-off__item
-                p.pay-off__label Créditos Prilov disponibles
-                p.pay-off__value 2.100 <small class="pay-off__small">Créditos</small>
-              .pay-off__item
-                label.pay-off__label(
-                  for='creditos-on') Usar en esta compra
-                .pay-off__group
-                  input.form__control.pay-off__control(
-                    id='creditos-on'
-                    type='text')
-                  small.pay-off__small Créditos
-            //-tipo de medio de pago
-            h3.subhead ¿Con que medio te gustaría pagar?
-            //-checkbox tipo pago
-            .boxcheck
-              .boxcheck__grid
-                //-item 1
-                .boxcheck__item
-                  input.form__input-radio(
-                    id="mercado",
-                    type="radio",
-                    name="envio",
-                    value="mercado",
-                    checked="checked")
-                  label.form__label.form__label_radio(
-                    for="mercado")
-                    span.boxcheck__label
-                      img(src="/static/img/logo-mercado-pago.png", alt="Pagar por medio de Mercado Pago")
-                //-end item 1
-                //-item 2
-                .boxcheck__item
-                  input.form__input-radio(
-                    id="PayU",
-                    type="radio",
-                    name="envio",
-                    value="PayU",
-                    checked="checked")
-                  label.form__label.form__label_radio(
-                    for="PayU")
-                    span.boxcheck__label
-                      img(src="/static/img/logo-payu.png", alt="Pagar por medio de PayU")
-                //-end item 2
-                //-item 3
-                .boxcheck__item
-                  input.form__input-radio(
-                    id="transferencia",
-                    type="radio",
-                    name="envio",
-                    value="transferencia",
-                    checked="checked")
-                  label.form__label.form__label_radio(
-                    for="transferencia")
-                    span.boxcheck__label
-                      span Transferencia Bancaria
-                //-end item 3
-
+          CompraEnvioPago
         //Tabla Total
         .list__side
           .list__frame
@@ -182,12 +66,12 @@
 
 <script>
 import CompraCarro from '@/components/CompraCarro'
-import UserData from '@/components/CompraFormUserData'
+import CompraEnvioPago from '@/components/CompraEnvioPago'
 export default {
   name: 'Compra',
   components: {
     CompraCarro,
-    UserData
+    CompraEnvioPago
   }
 }
 </script>
