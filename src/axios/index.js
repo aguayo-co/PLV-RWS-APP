27460f8d6b6e4e2d9a85eb446f0cc8cfa0603c96
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-03-27 12:12:23 -0500

    [Vue] Popups para errores por defecto en Axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -9,7 +9,28 @@ export default {
         Accept: 'application/json'
       }
     }
+
+    const baseErrorPopups = function (error) {
+      let modal = {
+        name: 'ModalMessage',
+        parameters: {
+          type: 'alert'
+        }
+      }
+      if (error.response && error.response.status === 401) {
+        modal.parameters.title = 'No estás autenticado.'
+        store.dispatch('user/logOut', modal)
+        store.dispatch('ui/showModal', modal)
+      }
+      if (error.response && error.response.status === 403) {
+        modal.parameters.title = 'No tiene permiso para esto.'
+        store.dispatch('ui/showModal', modal)
+      }
+      throw error
+    }
+
     Vue.axios = axios.create(baseOptions)
+    Vue.axios.interceptors.response.use(null, baseErrorPopups)
     Vue.prototype.$axios = Vue.axios
 
     // Authenticated Axios instance.
@@ -17,13 +38,14 @@ export default {
       ...baseOptions
     }
     Vue.axiosAuth = axios.create(authOptions)
+    Vue.axiosAuth.interceptors.response.use(null, baseErrorPopups)
     Vue.prototype.$axiosAuth = Vue.axiosAuth
     // We intercept every request and add the current Bearer token.
     Vue.axiosAuth.interceptors.request.use(function (config) {
       const token = store.getters['user/token']
 
       config.headers = config.headers || {}
-      config.headers.Authorization = 'Bearer ' + token
+      config.headers.Authorization = 'Bearers ' + token
       return config
     })
   }
