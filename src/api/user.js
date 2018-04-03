5a70d68db4e63545a19691492669e73f60dfdd50
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-04-03 12:42:38 -0500

    [Vue] LocalSTorage de Token y UserID consultados sólo en Vuex Storage

--- src/api/user.js
+++ src/api/user.js
@@ -5,17 +5,10 @@ import Vue from 'vue'
 
 export default {
   update: function (data) {
-    if (data.id === null) {
-      return Promise.reject(new Error('Need a user ID.'))
-    }
-    const payload = {...data}
-    return Vue.axiosAuth.patch('/api/users/' + data.id, payload)
+    return Vue.axiosAuth.patch('/api/users/' + data.id, data)
   },
 
   updateWithFile: function (data) {
-    if (data.id === null) {
-      return Promise.reject(new Error('Need a user ID.'))
-    }
     var formData = new FormData()
     Object.keys(data).forEach((key, index) => {
       if (key !== 'picture') {
@@ -88,11 +81,7 @@ export default {
     setTimeout(function () { return true }, 1000)
   },
 
-  load: function () {
-    const userId = window.localStorage.getItem('userId')
-    if (userId === null || window.localStorage.getItem('token') === null) {
-      return Promise.reject(new Error('No credentials found'))
-    }
+  load: function (userId) {
     return Vue.axiosAuth.get('/api/users/' + userId)
   }
 }
--- src/api/userAddresses.js
+++ src/api/userAddresses.js
@@ -9,9 +9,6 @@ export default {
   },
 
   update: function (data) {
-    if (window.localStorage.getItem('userId') === null || window.localStorage.getItem('token') === null) {
-      return Promise.reject(new Error('No credentials founds.'))
-    }
     if (data.user_id === null) {
       return Promise.reject(new Error('Need the address User ID.'))
     }
@@ -23,37 +20,24 @@ export default {
   },
 
   create: function (data) {
-    const userId = window.localStorage.getItem('userId')
-    if (userId === null || window.localStorage.getItem('token') === null) {
-      return Promise.reject(new Error('No credentials founds.'))
+    if (data.user_id === null) {
+      return Promise.reject(new Error('Need the address User ID.'))
     }
+
     const payload = {
       address: data.address,
       region: data.region,
       zone: data.zone
     }
 
-    return Vue.axiosAuth.post('/api/users/' + userId + '/addresses', payload)
+    return Vue.axiosAuth.post('/api/users/' + data.user_id + '/addresses', payload)
   },
 
-  load: function () {
-    const userId = window.localStorage.getItem('userId')
-    if (userId === null || window.localStorage.getItem('token') === null) {
-      return Promise.reject(new Error('No credentials founds.'))
-    }
+  load: function (userId) {
     return Vue.axiosAuth.get('/api/users/' + userId + '/addresses')
   },
 
   delete: function (data) {
-    if (window.localStorage.getItem('userId') === null || window.localStorage.getItem('token') === null) {
-      return Promise.reject(new Error('No credentials founds.'))
-    }
-    if (data.user_id === null) {
-      return Promise.reject(new Error('Need the address User ID.'))
-    }
-    if (data.id === null) {
-      return Promise.reject(new Error('Need the address ID.'))
-    }
     return Vue.axiosAuth.delete('/api/users/' + data.user_id + '/addresses/' + data.id)
   }
 }
