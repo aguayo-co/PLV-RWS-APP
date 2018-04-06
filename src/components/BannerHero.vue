8a326d688ad915f5c54e1852bcb0441acce42708
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-04-06 13:45:12 -0500

    [ Frontend-2: [Master] Prilov ]: Banner interno Estructura, componente

---
+++ src/components/BannerHero.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.banner-hero
+  .banner-hero__slot
+    h3.banner-hero__title Vestidos
+    h4.banner-hero__subtitle Cientos de estilos para ti
+    a.banner-hero__btn.btn 50% en ropa de invierno
+    a.banner-hero__btn.btn 20% en tu primera compra
+  picture.banner-hero__crop
+    source.banner-hero__img(media='(max-width: 640px)', srcset='/static/img/demo/banner-internal-m1.jpg')
+    img.banner-slider__img(src='/static/img/demo/banner-internal.jpg')
+</template>
+
+<script>
+
+export default {
+  name: 'BannerHero'
+}
+</script>
