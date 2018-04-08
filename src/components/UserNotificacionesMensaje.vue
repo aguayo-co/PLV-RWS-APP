399c9e962ec30dd05232987a4e5539c6033b329d
Geraldine Díaz <geraldine.diaz@aguayopublicidad.com>
2018-04-08 12:34:39 -0500

    Front-end [Prilov-app]: inicialización componente "UserNotificacionesMensaje"

--- src/components/UserNotificacionesMensaje.vue
+++ src/components/UserNotificacionesMensaje.vue
@@ -1,9 +1,7 @@
 <template lang="pug">
-section.single
-  .single__inner
-    header.single__header
-      h1.single__title Método de envío
-      p.single__subtitle Habilita los métodos de envío disponibles para todos tus productos
+.single__inner
+  header.single__header
+    h1.single__title Notificaciones
 
 </template>
 
