c0fcc8b036b0589eba598f8bc88dd62dc8234c05
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-12 02:51:26 -0500

    [Prilov] [Maquetación] -  Se elimina tu closet, conflicto con el componente user data

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,6 @@ import UserMetodoEnvio from '@/components/UserMetodoEnvio'
 import UserDatosBancarios from '@/components/UserDatosBancarios'
 import UserTusVentas from '@/components/UserTusVentas'
 import UserTusCompras from '@/components/UserTusCompras'
-import UserTuCloset from '@/components/UserTuCloset'
 import UserNotificaciones from '@/components/UserNotificaciones'
 import UserValoraciones from '@/components/UserValoraciones'
 import UserFavoritos from '@/components/UserFavoritos'
@@ -72,11 +71,6 @@ export default new Router({
           path: 'notificaciones',
           name: 'user-notificaciones',
           component: UserNotificaciones
-        },
-        {
-          path: 'tu-closet',
-          name: 'user-tu-closet',
-          component: UserTuCloset
         },
         {
           path: 'valoraciones',
