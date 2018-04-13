b6971212be6b15abc163e68c1e29c1c8277fd5c0
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-04-13 18:31:48 -0500

    [Vue][UserTusCompras] Mehjora flujo de comprobante de transferencia

--- src/components/UserCompra/js/Pendiente.js
+++ src/components/UserCompra/js/Pendiente.js
@@ -2,12 +2,25 @@ import Base from './Base'
 
 export default Base.merge({
   name: 'Pendiente',
+  data () {
+    return {
+      changeReceipt: false
+    }
+  },
   computed: {
     isTransfer () {
       return this.order.payments[0].gateway === 'Transfer'
     },
     hasReceipt () {
-      return this.order.payments[0].payment_receipt !== null
+      return this.order.payments[0].transfer_receipt !== null
+    },
+    canUploadReceipt () {
+      return this.isTransfer && (!this.hasReceipt || this.changeReceipt)
+    }
+  },
+  methods: {
+    newReceipt () {
+      this.changeReceipt = true
     }
   }
 })
