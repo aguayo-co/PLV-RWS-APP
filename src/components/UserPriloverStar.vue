d2fc404e82df02586aafc2b1e8009c83f2fbb53a
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-04-13 10:10:35 -0500

    [ Frontend-2: [front-end-yehimy] Prilov ]:  Flujo 3

--- src/components/UserPriloverStar.vue
+++ src/components/UserPriloverStar.vue
@@ -71,20 +71,6 @@ section.single
           .step-data__box
             a.link_underline(href='#') No quiero ser una Prilover Star
             //-end item 3
-      //- .single__box
-      //-   ol.single__list-rank
-      //-     li.single__item-rank Si tienes créditos, se te descontarán $15.000 automáticamente. Si no tienes créditos, puedes comprarlos.
-      //-     li.single__item-rank Al ser Prilover Star:
-      //-       ul.single__list
-      //-         li.single__item Tu perfil en Prilov aparecerá de los primeros en la sección "Prilovers".
-      //-         li.single__item Tu perfil tendrá un sello "Prilover STAR" y tus productos también.
-      //-         li.single__item Siempre estaremos pendiente de ti y contestaremos tus mensajes en tiempo record.
-      //-         li.single__item Tu productos serán promocionados en nuestros e-mail masivos y en redes sociales.
-      //-         li.single__item Si tus fotos y productos no cumplen con el requisito mínimo de aprobación (Revisar Manual de cómo sacar buenas fotos) no podremos permitirte ser una Prilover STAR.
-      //-   .single__row
-      //-     a(href='#').btn.btn_solid Canjear créditos y ser una Prilover Star
-      //-   .single__row
-      //-     a(href='#').link_underline No quiero ser una Prilover Star
     //- Step 1_c NO => ¿Usuaria paga con pasarela online?
     //- Flujo 1_c
     .single__step
@@ -168,11 +154,34 @@ section.single
     //- Flujo 2_c ** CASO 2 **
     .single__step
       .alert
-        p.alert__txt.i-alert-circle Ha ocurrido un problema,no pudimos validar el pago.
+        p.alert__txt.i-alert-circle Ha ocurrido un problema, no pudimos validar el pago.
       .step-data__box
         a.btn.btn_solid(href='#') Volverlo a intentar
       .step-data__box
         a.link_underline(href='#') No quiero ser una Prilover Star
+    //- Step 3 => Información sobre Prilover Star
+    //- Flujo 3
+    .single__step
+      .single__banner
+        .single__banner__slot.right
+          h3.single__banner__title.bg_white Puedes ser una PRILOVER STAR sólo por $15.000 anuales
+        picture.single__banner__crop
+          source.single__banner__img(media='(max-width: 640px)', srcset='/static/img/demo/banner-internal_closet-m1.jpg')
+          img.single__banner__img(src='/static/img/demo/banner-internal_closet.jpg')
+      .single__box
+        ol.single__list-rank
+          li.single__item-rank Si tienes créditos, se te descontarán $15.000 automáticamente. Si no tienes créditos, puedes comprarlos.
+          li.single__item-rank Al ser Prilover Star:
+            ul.single__list
+              li.single__item Tu perfil en Prilov aparecerá de los primeros en la sección "Prilovers".
+              li.single__item Tu perfil tendrá un sello "Prilover STAR" y tus productos también.
+              li.single__item Siempre estaremos pendiente de ti y contestaremos tus mensajes en tiempo record.
+              li.single__item Tu productos serán promocionados en nuestros e-mail masivos y en redes sociales.
+              li.single__item Si tus fotos y productos no cumplen con el requisito mínimo de aprobación (Revisar Manual de cómo sacar buenas fotos) no podremos permitirte ser una Prilover STAR.
+        .single__row
+          a(href='#').btn.btn_solid Canjear créditos y ser una Prilover Star
+        .single__row
+          a(href='#').link_underline No quiero ser una Prilover Star
 
 </template>
 
