475749f1caccaf8a499712ed907e16f4e3a90b88
Juan David Saab <juan.saab@aguayo.co>
2018-04-17 07:54:17 -0500

    [Vue] - Detalle de producto: se corrige esquema de URL para pasar Id de producto como parámetro

--- src/pages/Producto.vue
+++ src/pages/Producto.vue
@@ -1,19 +1,18 @@
  <template lang="pug">
 .layout-page
   ProductoDetalle(:product='product')
-  ProductoPack(:product='product')
   ProductoUser(:user='user')
   ProductoNotificaciones
   ProductoSlider
 </template>
 
 <script>
-import axios from 'axios'
 import ProductoDetalle from '@/components/ProductoDetalle'
 import ProductoPack from '@/components/ProductoPack'
 import ProductoUser from '@/components/ProductoUser'
 import ProductoNotificaciones from '@/components/ProductoNotificaciones'
 import ProductoSlider from '@/components/ProductoSlider'
+import productsAPI from '@/api/product'
 
 export default {
   name: 'Producto',
@@ -31,18 +30,11 @@ export default {
     }
   },
   created () {
-    axios.get('https://prilov.aguayo.co/api/products?filter[slug]=' + this.$route.params.slug, {
-    })
+    let id = this.$route.params.slug.split('__')[1]
+    productsAPI.getProductById(id)
       .then(response => {
         this.product = response.data.data[0]
-        axios.get('https://prilov.aguayo.co/api/users/' + this.product.user_id, {
-        })
-          .then(response => {
-            this.user = response.data
-          })
-          .catch(e => {
-            console.log('ERROR : ' + e)
-          })
+        this.user = this.product.user
       })
       .catch(e => {
         console.log('ERROR : ' + e)
