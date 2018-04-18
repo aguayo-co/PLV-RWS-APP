e67b8b97065b44ef1b08252ee1760404c1ba3007
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-04-18 17:18:40 -0500

    [ Frontend-2: [front-end-yehimy] Prilov ]:  Blog Banner

--- src/components/Blogbanner.vue
+++ src/components/Blogbanner.vue
@@ -6,16 +6,6 @@
   picture.banner-hero__crop
     source.banner-hero__img(media='(max-width: 640px)', srcset='static/img/demo/banner-blog-m.jpg')
     img.banner-slider__img(src='static/img/demo/banner-blog.jpg')
-//- .banner-hero
-//-   .banner-hero__slot
-//-     .banner-hero__head
-//-       h3.banner-hero__title {{ banner.title }}
-//-       h4.banner-hero__subtitle {{ banner.subtitle }}
-//-     .banner-hero__main
-//-       a.banner-hero__btn.btn(:href="banner.url") {{ banner.button_text }}
-//-   picture.banner-hero__crop
-//-     source.banner-hero__img(media='(max-width: 640px)', :srcset='banner.image')
-//-     img.banner-slider__img(:src='banner.image')
 </template>
 
 <script>
