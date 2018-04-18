e3b9207941a4685a990c8c50abfe3f6540817919
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-04-18 17:52:38 -0500

    [ Frontend-2: [front-end-yehimy] Prilov ]:  Blog Banner Prom

---
+++ src/components/BlogbannerProm.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+figure.blog-bannerprom.banner-hero
+  .banner-hero__slot
+    .banner-hero__head
+      figcaption.banner-hero__title Belleza Interior
+      span.banner-hero__subtitle Cientos de estilos para tí
+    .banner-hero__main
+      a.banner-hero__btn.btn(href='#') 50% en vestidos cortos
+  picture.banner-hero__crop
+    source.banner-hero__img(
+      media='(max-width: 640px)', srcset='static/img/demo/banner-internal-m1.jpg')
+    img.banner-slider__img(
+      src='static/img/demo/banner-internal.jpg')
+</template>
+
+<script>
+
+export default {
+  name: 'BlogbannerProm'
+}
+</script>
--- src/pages/Contenido.vue
+++ src/pages/Contenido.vue
@@ -2,17 +2,20 @@
 .layout-page
   BannerTop
   Blogbanner
+  BlogbannerProm
 
 </template>
 
 <script>
 import BannerTop from '@/components/BannerTop'
 import Blogbanner from '@/components/Blogbanner'
+import BlogbannerProm from '@/components/BlogbannerProm'
 
 export default {
   name: 'Contenido',
   components: {
     BannerTop,
+    BlogbannerProm,
     Blogbanner
   }
 }
