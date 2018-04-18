635ebf80948499e840038f67a4be519b11ec6d62
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-04-18 15:10:11 -0500

    [ Frontend-2: [front-end-yehimy] Prilov ]:  Componente: InstaShop.vue  color rosa del título

--- src/pages/instashop.vue
+++ src/pages/instashop.vue
@@ -3,7 +3,9 @@
   BannerTop
   section.section_product.prod-gridlight
     header.section__head
-      h1.title_head Instashop
+      h1.title_head
+        span.txt_brand Insta
+        | shop
       h2.title_subhead Encuentra TODO lo que publicamos en nuestro Instagram @prilovchile y cómpralo directamente
     .filtrate-box
       .filtrate__row.i-search
