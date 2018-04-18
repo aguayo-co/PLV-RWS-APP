252fa5d4a23da65ac8821fa374c0be570b7187cc
Geraldine Diaz <geraldine.diaz@aguayo.co>
2018-04-18 17:39:47 -0500

    [Front-end: Prilov]: marcado de blog-list en pagina de contenido

--- src/pages/Contenido.vue
+++ src/pages/Contenido.vue
@@ -1,8 +1,21 @@
 <template lang="pug">
 .layout-page
-  BannerTop
-  Blogbanner
-
+  .blog
+    BannerTop
+    Blogbanner
+    h4.blog__subhead Marcas disponibles en Prilov
+    ul
+      li Adidas
+      li Asos
+        ul
+          li 020
+          li Vestidos
+            ul
+              li Vestidos cortos
+      li Converse
+      li Forever 21
+      li Foster
+    h4.blog__subhead Las prendas más vendidas en Prilov
 </template>
 
 <script>
