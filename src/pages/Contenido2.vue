09ec9832bdc375018295a6ece2f52d5358b38224
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-19 05:02:08 -0500

    [Prilov] [Maquetación] -  Contenidos ¿Cómo cobro lo que gano con mis ventas?

--- src/pages/Contenido2.vue
+++ src/pages/Contenido2.vue
@@ -6,53 +6,6 @@
       header.blog__header
         h1.blog__title ¿Cómo cobro lo que gano con mis ventas?
         p.blog__kicker Todo lo que tienes que saber sobre el dinero que ganas cuando vendes en Prilov.
-      //- article.blog__body
-      //-   h2.blog__headline El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein salida de la mente de Raf Simons.
-      //-   .blog__spread
-      //-     .blog__copy
-      //-       p Ernesto Cervantes tuvo una oportunidad como nadie más en la industria: abrió el primer desfile de Raf Simons en Calvin Klein, formando parte de la nueva generación de la marca estadounidense bajo el mando del genio belga. De padres mexicanos, Ernesto nació en Orange County, pero creció en Perris, California, tiempo en el cual aprendió las virtudes de salirse de la cotidianidad y el confort que su entorno le ofrecía. Desde su lanzamiento como modelo exclusivo de Calvin Klein, también fue uno de los protagonistas en el video de The xx I Dare You, i-D platicó con él desde California para saber qué pensaba sobre representar su herencia mexicana en la nueva era de una marca conocida como uno de los pilares de la moda estadounidense.
-      //-       p.blog__byline
-      //-         a.link_underline(
-      //-         href='#') ¿Te gustaría comprar en Prilov?
-      //-     aside.blog-side
-      //-       dl
-      //-         dt ¿Cómo vender en prilov?
-      //-         dd Convierte tu clóset en tu propia tienda
-      //-         dt ¿Cómo Comprar en Prilov?
-      //-         dd La ropa que te gusta, es tuya
-      //-       dl
-      //-         dt Marcas
-      //-         dd
-      //-           a.link_underline(
-      //-             href='#') Adidas
-      //-         dd
-      //-           a.link_underline(
-      //-             href='#') Asos
-      //-         dd
-      //-           a.link_underline(
-      //-             href='#') Converse
-      //-         dd
-      //-           a.link_underline(
-      //-             href='#') Forever 21
-      //-         dd
-      //-           a.link_underline(
-      //-             href='#') Foster
-    BlogbannerProm
-    //- .blog__inner
-    //-   article.blog__body
-    //-     h2.blog__headline El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein salida de la mente de Raf Simons.
-    //-     .blog__spread
-    //-       .blog__copy
-    //-         p Ernesto Cervantes tuvo una oportunidad como nadie más en la industria: abrió el primer desfile de Raf Simons en Calvin Klein, formando parte de la nueva generación de la marca estadounidense bajo el mando del genio belga. De padres mexicanos, Ernesto nació en Orange County, pero creció en Perris, California, tiempo en el cual aprendió las virtudes de salirse de la cotidianidad y el confort que su entorno le ofrecía. Desde su lanzamiento como modelo exclusivo de Calvin Klein, también fue uno de los protagonistas en el video de The xx I Dare You, i-D platicó con él desde California para saber qué pensaba sobre representar su herencia mexicana en la nueva era de una marca conocida como uno de los pilares de la moda estadounidense.
-    //-       blockquote.blog__pullquote(
-    //-         cite='http://prilov.html')
-    //-         p.blog__quote El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein salida de la mente de Raf Simons.
-    //-     .blog__spread.blog__spread_upside
-    //-       .blog__copy
-    //-         p Ernesto Cervantes tuvo una oportunidad como nadie más en la industria: abrió el primer desfile de Raf Simons en Calvin Klein, formando parte de la nueva generación de la marca estadounidense bajo el mando del genio belga. De padres mexicanos, Ernesto nació en Orange County, pero creció en Perris, California, tiempo en el cual aprendió las virtudes de salirse de la cotidianidad y el confort que su entorno le ofrecía. Desde su lanzamiento como modelo exclusivo de Calvin Klein, también fue uno de los protagonistas en el video de The xx I Dare You, i-D platicó con él desde California para saber qué pensaba sobre representar su herencia mexicana en la nueva era de una marca conocida como uno de los pilares de la moda estadounidense.
-    //-       blockquote.blog__pullquote(
-    //-         cite='http://prilov.html')
-    //-         p.blog__quote El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein salida de la mente de Raf Simons.
     article.blog__body
       .blog__inner
         p.blog__lead Cuando vendas un producto, siempre se generarán créditos en tu cuenta Prilov. Puedes usarlos para comprar otra joyita o solicitar el pago a tu cuenta bancaria.
@@ -65,43 +18,18 @@
               span Te regalamos en tu primera compra
           .blog__copy
             p Cuando la compradora recibe su producto de forma satisfactoria y lo marque como "Recibido", tus créditos serán generados automáticamente a tu cuenta Prilov. Ese dinero puedes usarlo para seguir comprando lo que quieras en Prilov. Cuando hagas una compra,  el sistema descontará el monto que tienes en créditos al total de la compra que estás realizando. 
-      .blog__out
-        .blog__spread
-          figure.blog__picture
-            img.blog__image(src="static/img/demo/blog-002.jpg", alt="Alt imagen")
-            figcaption.blog__caption
-              span Te regalamos en tu primera compra
-          .blog__copy
-            p Ernesto Cervantes tuvo una oportunidad como nadie más en la industria: abrió el primer desfile de Raf Simons en Calvin Klein, formando parte de la nueva generación de la marca estadounidense bajo el mando del genio belga. De padres mexicanos, Ernesto nació en Orange County, pero creció en Perris, California, tiempo en el cual aprendió las virtudes de salirse de la cotidianidad y el confort que su entorno le ofrecía. Desde su lanzamiento como modelo exclusivo de Calvin Klein, también fue uno de los protagonistas en el video de The xx I Dare You, i-D platicó con él desde California para saber qué pensaba sobre representar su herencia mexicana en la nueva era de una marca conocida como uno de los pilares de la moda estadounidense.
+    BlogbannerProm
     .blog__inner
       article.blog__body
-        h2.blog__headline El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein salida de la mente de Raf Simons.
+        h2.blog__headline Traspasando créditos a cuenta bancaria
         .blog__copy
-          p Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi incidunt labore doloribus iure veniam, numquam cumque, iste laboriosam, obcaecati voluptates architecto assumenda delectus harum possimus praesentium reprehenderit unde dignissimos eaque!Tempore provident eveniet, dicta repellendus deleniti eligendi facere eum ut minus nihil temporibus earum exercitationem asperiores voluptas sapiente odio assumenda delectus dolorem debitis vero qui nesciunt sunt quae cupiditate. Quos.Ut praesentium repellat ab, fugit veniam illo aliquam. Sapiente aspernatur laborum quod animi expedita suscipit dignissimos minima consequuntur eius odio, impedit quisquam corporis mollitia debitis reprehenderit libero vel iusto nam.Ad deleniti aliquam et eos quam molestiae quibusdam eligendi, incidunt voluptas quasi, porro voluptatum accusamus eius dolorum saepe! Sed voluptatibus delectus nulla voluptatum culpa aliquam qui expedita officiis quis recusandae.Incidunt enim rerum odio, recusandae maiores vero dolore excepturi? Nulla facere et ipsum laudantium dolore exercitationem facilis rem praesentium. Fugiat vel dicta a, aspernatur dolorum sed ducimus quibusdam eveniet enim!
-          h4.blog__subhead Marcas disponibles en Prilov
-          ul
-            li Adidas
-            li Asos
-              ul
-                li 020
-                li Vestidos
-                  ul
-                    li Vestidos cortos
-            li Converse
-            li Forever 21
-            li Foster
-          h4.blog__subhead Las prendas más vendidas en Prilov
-          ol
-            li Chaquetas
-            li Vestidos
-            li Vestidos cortos
-            li Pantalones
-            li Zapatos
-            li Chaquetas
-            li Vestidos
-            li Vestidos cortos
-            li Pantalones
-            li Zapatos
+          p ¡Puedes transferir tus créditos a tu cuenta bancaria en cualquier momento!  Sólo recuerda que debes tener más de $4.000 para que sean transferidos. Para transferir tus créditos, debes hacer clic en "Tu cuenta" e ir a la sección "Créditos". Ahora puedes ver la opción "Convertir en dinero", la seleccionas y el dinero tardará entre 1 a 4 días hábiles en aparecer en la cuenta bancaria que ingresaste en tu cuenta Prilov.
+    .blog__inner
+      article.blog__body
+        h2.blog__headline Lo que gana Prilov
+        .blog__copy
+          p Prilov cobrará un 20% de comisión del valor de los productos por cada venta que se genere; generándose el 80% del valor de los productos en créditos, más el costo de despacho pagado por la compradora. Por ejemplo, si el precio del producto es $10.000 y el costo de despacho es $1.000, entonces generaremos créditos por $8.000 + $1.000 = $9.000. Transferir los créditos a tu cuenta bancaria no tiene costo.
+         
 </template>
 
 <script>
