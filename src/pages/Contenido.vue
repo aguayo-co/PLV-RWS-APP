61f9009f15b8c5120e1c8f43a40e2814dfcb9155
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-18 21:22:39 -0500

    [Prilov] [Maquetación] -  Contenidos, estilos títulos y encabezados

--- src/pages/Contenido.vue
+++ src/pages/Contenido.vue
@@ -1,9 +1,41 @@
 <template lang="pug">
 .layout-page
-  .blog
+  section.blog
     BannerTop
     Blogbanner
-    BlogSide
+    header.blog__header
+      h1.blog__title Ernesto cervantes es el modelo de calvin klein representando la herencia latina
+      p.blog__kicker El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein
+    article.blog__body
+      h2.headline El modelo de padres mexicanos es parte vital de la nueva generación de Calvin Klein salida de la mente de Raf Simons.
+      .blog__spread
+        .blog__copy
+          p Ernesto Cervantes tuvo una oportunidad como nadie más en la industria: abrió el primer desfile de Raf Simons en Calvin Klein, formando parte de la nueva generación de la marca estadounidense bajo el mando del genio belga. De padres mexicanos, Ernesto nació en Orange County, pero creció en Perris, California, tiempo en el cual aprendió las virtudes de salirse de la cotidianidad y el confort que su entorno le ofrecía. Desde su lanzamiento como modelo exclusivo de Calvin Klein, también fue uno de los protagonistas en el video de The xx I Dare You, i-D platicó con él desde California para saber qué pensaba sobre representar su herencia mexicana en la nueva era de una marca conocida como uno de los pilares de la moda estadounidense.
+          a.link_underline(
+            href='#') ¿Te gustaría comprar en Prilov?
+        aside.blog-side
+          dl
+            dt ¿Cómo vender en prilov?
+            dd Convierte tu clóset en tu propia tienda
+            dt ¿Cómo Comprar en Prilov?
+            dd La ropa que te gusta, es tuya
+          dl
+            dt Marcas
+            dd
+              a.link_underline(
+                href='#') Adidas
+            dd
+              a.link_underline(
+                href='#') Asos
+            dd
+              a.link_underline(
+                href='#') Converse
+            dd
+              a.link_underline(
+                href='#') Forever 21
+            dd
+              a.link_underline(
+                href='#') Foster
     BlogbannerProm
     h4.blog__subhead Marcas disponibles en Prilov
     ul
@@ -36,15 +68,13 @@
 import BannerTop from '@/components/BannerTop'
 import Blogbanner from '@/components/Blogbanner'
 import BlogbannerProm from '@/components/BlogbannerProm'
-import BlogSide from '@/components/BlogSide'
 
 export default {
   name: 'Contenido',
   components: {
     BannerTop,
     BlogbannerProm,
-    Blogbanner,
-    BlogSide
+    Blogbanner
   }
 }
 
