0aa4c166550bba89a9d84bafc212631b455fd6f2
Juan David Saab <juan.saab@aguayo.co>
2018-04-20 12:02:06 -0500

    [Vue] - fixes issue #20 utilizando el esquema Axios.Auth centralizado. Pendiente maquetar página de respuesta pendiente.

--- src/api/product.js
+++ src/api/product.js
@@ -4,54 +4,19 @@
 import Vue from 'vue'
 
 export default {
-  create: function (product, imageBlobs, userId, userToken) {
-    var data = new FormData()
-    const headers = {
-      headers: {
-        'Authorization': 'Bearer ' + userToken
+  create: function (product) {
+    console.log(product)
+    var formData = new FormData()
+    Object.keys(product).forEach((key) => {
+      if (key === 'images' || key === 'color_ids') {
+        product[key].forEach((item) => {
+          formData.append(key + '[]', item)
+        })
+      } else {
+        formData.append(key, product[key])
       }
-    }
-
-    data.append('title', product.title)
-    data.append('description', product.description)
-    data.append('dimensions', product.dimensions)
-    data.append('original_price', product.originalPrice)
-    data.append('price', product.price)
-    data.append('commission', product.commission)
-    data.append('user_id', userId)
-    data.append('brand_id', product.brand_id)
-    data.append('category_id', product.subcategory_id)
-    data.append('size_id', 2)
-    data.append('color_ids', product.color_ids)
-    data.append('condition_id', product.condition_id)
-
-    product.color_ids.forEach(function (id, index) {
-      data.append('color_ids[]', id)
     })
-
-    imageBlobs.forEach(function (image, index) {
-      // convert base64 to raw binary data held in a string
-      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-      var byteString = atob(image.split(',')[1])
-
-      // separate out the mime component
-      var mimeString = image.split(',')[0].split(':')[1].split(';')[0]
-
-      // write the bytes of the string to an ArrayBuffer
-      var ab = new ArrayBuffer(byteString.length)
-      var ia = new Uint8Array(ab)
-      for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i)
-      }
-
-      // write the ArrayBuffer to a blob, and you're done
-      var bb = new Blob([ab], {type: mimeString})
-      var file = new File([bb], 'image' + index + '.png')
-      console.log(file)
-      data.append('images[]', file)
-    })
-
-    return Vue.axios.post('/api/products', data, headers)
+    return Vue.axiosAuth.post('/api/products', formData)
   },
 
   getProducts: function (page, items, filter, order) {
@@ -99,25 +64,5 @@ export default {
 
   getAllSizes: function () {
     return Vue.axios.get('/api/sizes')
-  },
-
-  dataURItoBlob: function (dataURI) {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    var byteString = atob(dataURI.split(',')[1])
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-    // write the bytes of the string to an ArrayBuffer
-    var ab = new ArrayBuffer(byteString.length)
-    var ia = new Uint8Array(ab)
-    for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i)
-    }
-
-    // write the ArrayBuffer to a blob, and you're done
-    var bb = new Blob([ab], {type: mimeString})
-    return bb
   }
 }
