4107c28ef2e06bbdc9e95be8e4b9b74d17a506ff
FRONT END 2 <yehimy.rincon@aguayopublicidad.com>
2018-04-23 16:52:06 -0500

    [ Frontend-2: [front-end-yehimy] Prilov ]:  Botón Publicar un venta va con posición sticky en el home

--- src/components/ButtonSticky.vue
+++ src/components/ButtonSticky.vue
@@ -1,10 +1,34 @@
 <template lang="pug">
-  .btn-sticky
+  .btn-sticky(v-if='scroll == 2 || scroll != null',
+  :class="{'js-btn_fade' : scroll == 1 }")
     a.btn-sticky__link.i-bag(href='#') Comienza a vender
 </template>
 
 <script>
 export default {
-  name: 'ButtonSticky'
+  name: 'ButtonSticky',
+  data () {
+    return {
+      scroll: 2,
+      scrollDetect: undefined,
+      lastScrollTop: 50
+    }
+  },
+  created () {
+    window.addEventListener('scroll', this.btnFade)
+  },
+  methods: {
+    btnFade: function () {
+      this.scrollDetect = window.pageYOffset || document.documentElement.scrollHeight
+      if (this.scrollDetect < this.lastScrollTop) {
+        this.scroll = 0
+        console.log(this.lastScrollTop + 'en cero')
+      } else {
+        this.scroll = 1
+        console.log(this.scrollDetect + 'en 1')
+      }
+      this.lastScrollTop = this.scrollDetect
+    }
+  }
 }
 </script>
