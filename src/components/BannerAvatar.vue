f9ec22c2748e186db75346ac18b5d7f5192dcfdc
Juan David Saab <juan.saab@aguayo.co>
2018-04-24 13:41:51 -0500

    [Vue] - Banner Avatar conectado.

--- src/components/BannerAvatar.vue
+++ src/components/BannerAvatar.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
 .banner-hero.banner-hero_user
-  picture.banner-hero__crop
-    source.banner-hero__img(media='(max-width: 640px)', srcset='/static/img/demo/banner-avatar-m.jpg')
-    img.banner-slider__img(src='/static/img/demo/banner-avatar.jpg')
+  picture.banner-hero__crop(v-if="user.cover")
+    source.banner-hero__img(media='(max-width: 640px)', :srcset='user.cover')
+    img.banner-slider__img(:src='user.cover')
+  picture.banner-hero__crop(v-else)
+      source.banner-hero__img(media='(max-width: 640px)', srcset='/static/img/demo/banner-avatar-m.jpg')
+      img.banner-slider__img(src='/static/img/demo/banner-avatar.jpg')
   .user-hero
     .user-hero__item
       .user-hero__head
         .user-data__avatar.user-data__avatar_border
           img.user-data__img(
-          src="/static/img/demo/user-avatar-001.jpg")
-        span.user-data__group.i-star-on Prilover
-          strong.txt_brand Star
+            v-if="user.picture"
+            :src="user.picture")
+          span.tool-user__letter(v-if="!user.picture && user.first_name") {{ user.first_name.charAt(0) }}
     .user-hero__item
-      h3.user-data__title Damarys Saldaña
+      h3.user-data__title {{ user.first_name }} {{ user.last_name }}
       .user-hero__notify
         ul.user-data__list
           li.user-data__value.i-like 20
@@ -33,6 +36,7 @@
 <script>
 
 export default {
+  props: ['user'],
   name: 'BannerAvatar'
 }
 </script>
