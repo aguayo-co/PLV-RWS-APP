246870458505484ef02995e0940d628f96c810e6
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-25 17:02:05 -0500

    [Prilov] [Maquetación] -  Ajustes marcado avatar de chat, se incluye un contenedor

--- src/components/ProductoPreguntas.vue
+++ src/components/ProductoPreguntas.vue
@@ -9,10 +9,12 @@
           .chat__group
             .chat-line
               .chat__bubble-main
-                img.chat-bubble__img(src="static/img/demo/user-avatar.jpg", alt="Avatar")
+                .chat-bubble__avatar
+                  img.chat-bubble__img(src="static/img/demo/user-avatar.jpg", alt="Avatar")
                 p.chat-bubble__txt Hola, si está aun disponible, ¿Quieres comprarlo?
               .chat__bubble
-                img.chat-bubble__img(src="static/img/demo/user-avatar-001.jpg", alt="Avatar")
+                .chat-bubble__avatar
+                  img.chat-bubble__img(src="static/img/demo/user-avatar-001.jpg", alt="Avatar")
                 p.chat-bubble__txt Hola, si, me interesa comprar.
               span.editor__btn(
                 v-if="editorAnswer == false",
@@ -33,7 +35,8 @@
           .chat__group
             .chat-line
               .chat__bubble-main
-                img.chat-bubble__img(src="static/img/demo/user-avatar-002.jpg", alt="Avatar")
+                .chat-bubble__avatar
+                  img.chat-bubble__img(src="static/img/demo/user-avatar-002.jpg", alt="Avatar")
                 p.chat-bubble__txt Disculpa, viajo a Argentina mañana en la tarde, será posible que lo valla yo a buscar directamente, por favor.. es para llevarlo al mencionado país, muchas gracias...
               span.editor__btn(
                 v-if="editorAnswer == false",
@@ -43,8 +46,7 @@
               form.editor__form
                 .chat__form-group
                   textarea.form__textarea.editor__textarea(
-                    rows="1"
-                  )
+                    rows="1")
                   .chat__btn-group
                     button.chat__btn-solid.i-shipping(
                       type="submit",
