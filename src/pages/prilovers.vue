76ce27e38303102d55d71247d68c2df97b9dd6d0
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-25 00:44:40 -0500

    [Prilov] [Maquetación] -  Recuperación estilos y cambios marcado Prilovers

--- src/pages/prilovers.vue
+++ src/pages/prilovers.vue
@@ -1,24 +1,28 @@
 <template lang="pug">
 .layout-page
   BannerTop
-  .section_product
-    article.card(v-for="user in prilovers")
-      figure.card__circle
-        img.card__photo(
-          v-if="user.picture"
-          :src='user.picture',
-          alt='')
-      h2.card__title
-      p.card__meta {{ user.published_products_count }} Productos
-      ul.user-data__list
-        li.user-data__value.i-like {{ user.followers_count }}
-        li.user-data__value.i-like.i_flip 0
-        li.user-data__value.i-less-circle 0
-      .card__group
-        .slot__group.i-it-girl(
-          v-show="true") It <span class="txt_brand">girl</span>
-        .slot__group.i-star-on(
-          v-show="false") Prilover <span class="txt_brand">Star</span>
+  .layout-inner
+    .card__grid
+      article.card(v-for="user in prilovers")
+        figure.card__circle
+          img.card__photo(
+            v-if="user.picture"
+            :src='user.picture',
+            alt='')
+        h2.card__name {{ user.first_name }} {{ user.last_name }}
+        p.card__meta.i-bag {{ user.published_products_count }} Productos
+        ul.user-data__list
+          li.user-data__value.i-like {{ user.followers_count }}
+          li.user-data__value.i-like.i_flip 0
+          li.user-data__value.i-less-circle 0
+        .card__group
+          .slot__group.i-it-girl(
+            v-show="true") It <span class="txt_brand">girl</span>
+          .slot__group.i-star-on(
+            v-show="false") Prilover <span class="txt_brand">Star</span>
+    .section_product__footer
+      p.btn__wrapper
+        a.btn Ver más Prilovers
 </template>
 
 <script>
