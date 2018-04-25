da170c0367bdf0aff5bc72ef90de5c7b509df8ae
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-25 11:54:29 -0500

    [Prilov] [Maquetación] -  Cambios de estructura chat pregunte a la vendedora

--- src/components/ProductoNotificaciones.vue
+++ src/components/ProductoNotificaciones.vue
@@ -2,7 +2,7 @@
 .layout-inner
   section.chat-slot
     header.single__header
-      h2.subhead Últimas preguntas
+      h2.subhead Preguntas sobre esta prenda
     .chat-slot__block
       .chat
         .chat__message_inner
@@ -14,41 +14,13 @@
               img.chat-bubble__img(src="static/img/demo/user-avatar-001.jpg", alt="Avatar")
               p.chat-bubble__txt Hola, si, me interesa comprar.
               span.chat-bubble__noti.i-check
-          .chat-line
-            .chat__bubble-main
-              img.chat-bubble__img(src="static/img/demo/user-avatar.jpg", alt="Avatar")
-              p.chat-bubble__txt Hola, si está aun disponible, ¿Quieres comprarlo?
-            .chat__bubble
-              img.chat-bubble__img(src="static/img/demo/user-avatar-001.jpg", alt="Avatar")
-              p.chat-bubble__txt Hola, si, me interesa comprar.
-              span.chat-bubble__noti.i-check
-          .chat-line
-            .chat__bubble-main
-              img.chat-bubble__img(src="static/img/demo/user-avatar.jpg", alt="Avatar")
-              p.chat-bubble__txt Hola, si está aun disponible, ¿Quieres comprarlo?
-            .chat__bubble
-              img.chat-bubble__img(src="static/img/demo/user-avatar-001.jpg", alt="Avatar")
-              p.chat-bubble__txt Hola, si, me interesa comprar.
-              span.chat-bubble__noti.i-check
-          .chat-line
-            .chat__bubble-main
-              img.chat-bubble__img(src="static/img/demo/user-avatar.jpg", alt="Avatar")
-              p.chat-bubble__txt Listo, ¿tienes alguna duda sobre el zapato?
-            .chat__bubble
-              img.chat-bubble__img(src="static/img/demo/user-avatar-001.jpg", alt="Avatar")
-              p.chat-bubble__txt Si, me interesa saber cuánto tiempo  tiene de uso.
-              span.chat-bubble__noti.i-check
-        .chat-inner
-          form.chat__form
-            label.chat__label Escribe un mensaje aquí
-            .chat__form-group
-              textarea.form__textarea.chat__textarea
-              .chat__btn-group
-                button.chat__btn-solid.i-shipping(type="submit")
-                //- .chat__btn-file
-                //-   input#chatFile.chat__btn-flat(type='file')
-                //-   label(for="chatFile").i-blog.chat__label-flat.chat__btn
-                //- a.i-smile.chat__btn(href="#")
+            .editor
+              form.editor__form
+                .chat__form-group
+                  textarea.form__textarea.editor__textarea
+                  .chat__btn-group
+                    button.chat__btn-solid.i-shipping(
+                      type="submit")
       .chat-notify
         form.chat__form
           label.chat__label ¡Pregúntale a la vendedora!
