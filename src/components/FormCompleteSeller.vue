ac43f873d920245f0ab12690dd88cc692f918696
Geraldine Díaz <geraldine.diaz@aguayopublicidad.com>
2018-04-27 16:46:02 -0500

    Fix #50 Ajustado botón de eliminar en croppa

--- src/components/FormCompleteSeller.vue
+++ src/components/FormCompleteSeller.vue
@@ -14,9 +14,10 @@
           .upfile__small
             h3.upfile__title Foto de perfil
             .upfile__item
-              a.delete(
+              a.upfile__delete.i-x(
                 v-show='toggleImageDelete',
-                @click='removeImage') Eliminar
+                @click='removeImage')
+                span.hide Eliminar
               span.help(
                 v-if="errorLog.picture"
               ) {{ errorLog.picture }}
