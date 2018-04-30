cf6c57ae50c6e506ce34bd44f078c610c9b235ab
Andrea Acosta Becerra <andrea.acosta@aguayopublicidad.com>
2018-04-30 07:05:41 -0500

    Fix Isuue #39: Se implementan estilos .alert-msg para mensaje

--- src/components/ProductoDetalle.vue
+++ src/components/ProductoDetalle.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .layout-inner
-  p(v-if="isOwner") Este es es un producto de tu closet
-    router-link(:to="'/closet/' + user.id") Ir a mi Closet
+  .alert-msg.alert-msg_center
+    p(v-if="isOwner") Este es es un producto de tu closet <router :to="'/closet/' + user.id" class="link_underline"> Ir a mi Closet</router>
   article.detail
     .detail__gallery
       figure.detail__picture
