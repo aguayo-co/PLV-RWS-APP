a8c85685b6b88c7ea214fc6b23ae617c2cafdc7b
Juan David Saab <juan.saab@aguayo.co>
2018-05-01 14:36:43 -0500

    [Minor] - Correcciones de errores de Lint

--- src/components/ProductoUser.vue
+++ src/components/ProductoUser.vue
@@ -10,12 +10,12 @@
         .user-header__item.user-item_gutter
           .user-data__avatar.user-data__avatar_small
             img.user-data__img(
-              v-if="user.picture"
-              :src="user.picture",
-              :alt="'Perfil' + ' ' + first_name")
-            span.profile__letter(v-if="!user.picture && user.first_name") {{ user.first_name.charAt(0) }}
+              v-if="userData.picture"
+              :src="userData.picture",
+              :alt="'Perfil' + ' ' + userData.first_name")
+            span.profile__letter(v-if="!userData.picture && userData.first_name") {{ userData.first_name.charAt(0) }}
         .user-header__item.user-iten_grow
-          h3.user-data__title {{ user.first_name }} {{ user.last_name }}
+          h3.user-data__title {{ userData.first_name }} {{ userData.last_name }}
           .user-data__subitem
             //-Notificaciones
             .user-data__block
@@ -24,11 +24,11 @@
                 li.user-data__value.i-like.i_flip 100
                 li.user-data__value.i-less-circle 100
               ul.user-data__list
-                li.user-data__track {{ followers_count }} Seguidores
-                li.user-data__track {{ following_count }} Siguiendo
-                li.user-data__track(v-if="user.id")
+                li.user-data__track {{ userData.followers_count }} Seguidores
+                li.user-data__track {{ userData.following_count }} Siguiendo
+                li.user-data__track(v-if="userData.id")
                   router-link.i-email(
-                    :to="{ name: 'privateMessage', params: { recipientId: user.id }}",
+                    :to="{ name: 'privateMessage', params: { recipientId: userData.id }}",
                     title='Enviar mensaje privado') Enviar mensaje privado
             .user-data__rating
               .chat__line
@@ -41,11 +41,13 @@
                       p.chat-bubble__txt Excelente vendedora. Todo rápido y confiable
 </template>
 <script>
-import { mapState } from 'vuex'
 export default {
   name: 'ProductoUser',
   props: ['user'],
   computed: {
+    userData () {
+      return this.user
+    }
   }
 }
 </script>
