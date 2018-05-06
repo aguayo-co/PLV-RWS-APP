e15ac8821e404c7120e9eb96e531f33b34aad1db
Juan David Saab <juan.saab@aguayo.co>
2018-05-06 08:22:58 -0500

    [VACATIONS-MODE] - Se implementa Vacations Mode.

--- src/components/UserVacaciones.vue
+++ src/components/UserVacaciones.vue
@@ -3,14 +3,15 @@
   .box-flat__grid
     .box-flat__lead.i-trip
       .box-flat__lead-group
-        h3.box-flat__title ¿Te vas de vacaciones?
-        p.box__txt Recuerda que si te vas de vacaciones o vas a estar sin conexión por un tiempo, habilita el "Modo Vacaciones" y tus productos no estarán disponibles para la venta.
+        h3.box-flat__title Modo vacaciones
+        p.box__txt Recuerda que si te vas de vacaciones o vas a estar sin conexión por un tiempo, puedes habilitar esta opción y tus productos no estarán disponibles para la venta.
     .box__switch
       .form__switch
         input.switch__input(
+          @change="setMode"
           type="checkbox",
           id="standard",
-          checked)
+          v-model="enabled")
         label.switch__label(
           for="standard")
             span.switch__status
@@ -18,8 +19,56 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
-  name: 'UserVacaciones'
+  name: 'UserVacaciones',
+  data () {
+    return {
+      enabled: false
+    }
+  },
+  computed: {
+    ...mapState('user', [
+      'id',
+      'vacation_mode'
+    ])
+  },
+  methods: {
+    setMode: function () {
+      const data = {
+        vacation_mode: this.enabled
+      }
+      this.$store.dispatch('user/update', data)
+        .then(response => {
+          let productStatus = ''
+          this.enabled ? productStatus = 'deshabilitados' : productStatus = 'habilitados'
+          const modal = {
+            name: 'ModalMessage',
+            parameters: {
+              type: 'positive',
+              title: 'Actualizaste tu modo vacaciones. Todos tus productos han quedado ' + productStatus
+            }
+          }
+          this.$store.dispatch('ui/showModal', modal)
+        })
+        .catch(e => {
+          const modal = {
+            name: 'ModalMessage',
+            parameters: {
+              type: 'alert',
+              title: 'Ocurrió un error al actualizar tu modo vacaciones'
+            }
+          }
+          this.$store.dispatch('ui/showModal', modal)
+          this.enabled = this.vacations_mode
+        })
+    }
+  },
+  watch: {
+    id: function () {
+      this.enabled = this.vacation_mode
+    }
+  }
 }
 </script>
