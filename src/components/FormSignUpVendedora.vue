4318ea9eb4fc2705e37a50ee41f122801da8bcff
Juan David Saab <juan.saab@aguayo.co>
2018-05-19 12:45:15 -0500

    [PUBLISH] - Deshabilita botones de submit al comunicarse con el back. Deshabilita validación de contraseña: Fix issue #68

--- src/components/FormSignUpVendedora.vue
+++ src/components/FormSignUpVendedora.vue
@@ -24,9 +24,9 @@
               .upfile__label
                 .upfile__text.i-upload(
                   v-if="mqDesk") Arrastra una foto o
-                .upfile__btn Sube una imagen
+                button.upfile__btn(ref="picture") Sube una imagen
               croppa(
-                v-model='picture',
+                v-model='newUser.picture',
                 :width='300',
                 :height='300',
                 :quality='2',
@@ -37,41 +37,19 @@
         .form-section__inner
           p.form__mesagge Ups! Vemos que aún no tienes una cuenta. Te crearemos una cuenta rápidamente para que puedas continuar con la publicación de tu venta. Por favor ingresa los siguientes datos:
           .form__row(
-            :class='{ "is-danger": errorLog.nombre }')
-            label.form__label(
-              for='nombre') Nombre
-            span.help(
-              v-if="errorLog.nombre") {{ errorLog.nombre }}
-            input.form__control(
-              v-model='nombre',
-              id='username',
-              type='text',
-              data-vv-name='nombre'
-            )
-          .form__row(:class='{ "is-danger": errorLog.apellidos }')
-            label.form__label(
-              for='apellidos') Apellidos
-            span.help(
-              v-if="errorLog.apellidos") {{ errorLog.apellidos }}
-            input.form__control(
-              v-model='apellidos',
-              id='userLast',
-              type='text',
-              data-vv-name='apellidos')
-          .form__row(
             :class='{ "is-danger": errorLog.email }')
             label.form__label(
               for='emailUser') Correo
             span.help(
               v-show='errorLog.email') {{ errorLog.email }}
             input.form__control(
-              v-model='email',
+              @keyup='errorLog.email = undefined',
+              ref="email",
+              v-model='newUser.email',
               id='emailUser',
               type='email',
-              data-vv-name='email')
-            span.help(
-              v-if='infoTexts.emailExist') {{ infoTexts.emailExist }}
-              a(href='#') ¡Recupérala aquí!
+              disabled)
+            p.form__note ¿Ingresaste mal tu email en el paso anterior? <a class="link_underline" @click="$router.go()">Cámbialo aquí</a>
           .form__row(
             :class='{ "is-danger": errorLog.emailConfirm }')
             label.form__label(
@@ -79,7 +57,7 @@
             span.help(
               v-if="errorLog.emailConfirm") {{ errorLog.emailConfirm }}
             input.form__control(
-              v-model='emailConfirm',
+              v-model='newUser.emailConfirm',
               id='emailConfirm',
               type='email',
               data-vv-name='emailConfirm')
@@ -91,7 +69,9 @@
               v-if="errorLog.password") {{ errorLog.password }}
             .form__password
               input.form__control(
-                v-model='password',
+                @keyup='errorLog.password = undefined',
+                ref="password",
+                v-model='newUser.password',
                 id='passwordUser',
                 :type="viewPass ? 'text' : 'password'",
                 data-vv-name='password',
@@ -99,17 +79,42 @@
               span.form__visible.i-view(
                 @click='visiblePass')
             span.password-bar(
-              v-if='errorLog.passwordDetail.length > 0',
+              v-if="this.newUser.password",
               :class='"level-"+(3-errorLog.passwordDetail.length)')
-            div.helper(
+            //- div.helper(
               v-if='errorLog.passwordDetail.length > 0' )
               ul.helper__list
                 li(
                   v-for='detail in errorLog.passwordDetail') {{ detail }}
+          .form__row(
+            :class='{ "is-danger": errorLog.nombre }')
+            label.form__label(
+              for='nombre') Nombre
+            span.help(
+              v-if="errorLog.nombre") {{ errorLog.nombre }}
+            input.form__control(
+              @keyup='errorLog.nombre = undefined',
+              ref="nombre",
+              v-model='newUser.nombre',
+              id='username',
+              type='text',
+              data-vv-name='nombre')
+          .form__row(:class='{ "is-danger": errorLog.apellidos }')
+            label.form__label(
+              for='apellidos') Apellidos
+            span.help(
+              v-if="errorLog.apellidos") {{ errorLog.apellidos }}
+            input.form__control(
+              @keyup='errorLog.apellidos = undefined',
+              ref="apellidos",
+              v-model='newUser.apellidos',
+              id='userLast',
+              type='text',
+              data-vv-name='apellidos')
           .form-section.form-section_footer
             .form__row.form__row_away
               button.btn.btn_solid(
-                @click.prevent='validateBeforeSubmit') Registrarse
+                @click.prevent='signUp($event)') Registrarse
         //- .break
         //-   span.break__txt O
 </template>
@@ -128,18 +133,13 @@ export default {
   props: ['email'],
   data () {
     return {
-      nombre: '',
-      apellidos: '',
-      emailConfirm: '',
-      password: '',
+      newUser: {},
       flagSignUp: 'SignUp',
       errorLog: {
         passwordDetail: []
       },
       infoTexts: {},
       regionsList: {},
-      picture: null,
-      pictureURL: null,
       toggleImageDelete: false,
       viewPass: false
     }
@@ -148,95 +148,98 @@ export default {
     visiblePass: function () {
       this.viewPass = !this.viewPass
     },
-    signUp: function () {
-      // console.log(this.$store.get('userAuth'))
-      const payload = {
-        first_name: this.nombre,
-        last_name: this.apellidos,
-        email: this.email,
-        password: this.password,
-        picture: this.pictureURL
-      }
-      const modal = {
-        name: 'ModalMessage',
-        parameters: {
-          type: 'preload',
-          title: '¡Gracias! Estamos creando tu cuenta'
+    signUp: function (event) {
+      event.target.disabled = true
+
+      this.validateBeforeSubmit()
+      if (Object.keys(this.errorLog).length === 1) {
+        const payload = {
+          first_name: this.newUser.nombre,
+          last_name: this.newUser.apellidos,
+          email: this.newUser.email,
+          password: this.newUser.password,
+          picture: this.newUser.pictureURL
         }
-      }
-      this.$store.dispatch('ui/showModal', modal)
-      userAPI.create(payload)
-        .then(response => {
-          this.$store.dispatch('user/setUser', response.data)
-            .then(() => {
-              this.$store.dispatch('ui/closeModal')
-              this.$store.dispatch('user/loadUser')
-              this.migrateCart()
-            }).catch((e) => {
-              console.log(e)
-            })
-        })
-        .catch(e => {
-          this.$store.dispatch('ui/closeModal')
-          if (e.response.data.errors.exists) {
-            this.infoTexts.emailExist = 'Parece que este email ya está siendo usado. ¿Olvidaste tu contraseña?'
-            this.validatePassword()
-          } else {
-            const modal = {
-              name: 'ModalMessage',
-              parameters: {
-                type: 'alert',
-                title: '¡Ups! Parece que ocurrió un error',
-                body: Object.values(e.response.data.errors)[0]
-              }
-            }
-            this.$store.dispatch('ui/showModal', modal)
+        const modal = {
+          name: 'ModalMessage',
+          parameters: {
+            type: 'preload',
+            title: '¡Gracias! Estamos creando tu cuenta'
           }
-          this.validatePassword()
-        })
+        }
+        this.$store.dispatch('ui/showModal', modal)
+        userAPI.create(payload)
+          .then(response => {
+            this.$store.dispatch('user/setUser', response.data)
+              .then(() => {
+                this.$store.dispatch('ui/closeModal')
+                this.$store.dispatch('user/loadUser')
+                this.migrateCart()
+              }).catch((e) => {
+                console.log(e)
+              })
+          })
+          .catch(e => {
+            this.$store.dispatch('ui/closeModal')
+            if (e.response.data.errors.exists) {
+              this.infoTexts.emailExist = 'Parece que este email ya está siendo usado. ¿Olvidaste tu contraseña?'
+              this.validatePassword()
+            } else {
+              const modal = {
+                name: 'ModalMessage',
+                parameters: {
+                  type: 'alert',
+                  title: '¡Ups! Parece que ocurrió un error',
+                  body: Object.values(e.response.data.errors)[0]
+                }
+              }
+              this.$store.dispatch('ui/showModal', modal)
+            }
+            this.validatePassword()
+          })
+      } else {
+        event.target.disabled = false
+      }
     },
-    validateBeforeSubmit: function (e) {
+    validateBeforeSubmit: function () {
       this.errorLog = {
         passwordDetail: []
       }
-      this.infoTexts = {}
-
-      if (!this.nombre) this.errorLog.nombre = 'Debes ingresar tu nombre'
-      if (!this.apellidos) this.errorLog.apellidos = 'Debes ingresar tus apellidos'
-      if (!this.email) {
+      if (!this.newUser.password) this.errorLog.password = 'Debes ingresar una contraseña'
+      if (!this.newUser.nombre) this.errorLog.nombre = 'Debes ingresar tu nombre'
+      if (!this.newUser.apellidos) this.errorLog.apellidos = 'Debes ingresar tus apellidos'
+      if (!this.newUser.email) {
         this.errorLog.email = 'Debes ingresar tu email'
       } else {
-        if (!/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/.test(this.email)) {
+        if (!/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/.test(this.newUser.email)) {
           this.errorLog.email = 'El email que ingresaste no parece válido.'
         }
       }
-      if (!this.email) this.errorLog.emailConfirm = 'Debes ingresar de nuevo tu email'
-      if (this.email !== this.emailConfirm) this.errorLog.emailConfirm = 'Este email no coincide con el primero que ingresaste'
+      if (!this.newUser.email) this.errorLog.emailConfirm = 'Debes ingresar de nuevo tu email'
+      if (this.newUser.email !== this.newUser.emailConfirm) this.errorLog.emailConfirm = 'Este email no coincide con el primero que ingresaste'
+      if (!this.newUser.picture.hasImage()) this.errorLog.picture = 'Debes cargar una imagen para tu perfil'
 
-      this.validatePassword(e)
+      if (Object.keys(this.errorLog)[0] !== 'passwordDetail') this.$refs[Object.keys(this.errorLog)[0]].focus()
+      if (Object.keys(this.errorLog)[1]) this.$refs[Object.keys(this.errorLog)[1]].focus()
 
-      if (Object.keys(this.errorLog).length === 1 && this.errorLog.passwordDetail.length === 0) {
-        this.signUp()
-      }
+      this.validatePassword()
     },
-    validatePassword: function (e) {
+    validatePassword: function () {
       this.errorLog.passwordDetail = []
 
-      if (!this.password) this.errorLog.password = 'Debes ingresar una contraseña'
-      if (this.password.length < 8) this.errorLog.passwordDetail.push('Tu contraseña debe tener al menos 8 caracteres')
-      if (!/[a-zA-Z]/.test(this.password)) this.errorLog.passwordDetail.push('Tu contraseña debe contener al menos una letra')
-      if (!/\d+/.test(this.password)) this.errorLog.passwordDetail.push('Tu contraseña debe contener al menos un número')
-      if (!this.picture.hasImage()) this.errorLog.picture = 'Debes cargar una imagen para tu perfil'
+      if (this.newUser.password && this.newUser.password.length < 8) this.errorLog.passwordDetail.push('Tu contraseña debe tener al menos 8 caracteres')
+      if (!/[a-zA-Z]/.test(this.newUser.password)) this.errorLog.passwordDetail.push('Tu contraseña debe contener al menos una letra')
+      if (!/\d+/.test(this.newUser.password)) this.errorLog.passwordDetail.push('Tu contraseña debe contener al menos un número')
     },
     handlePicture: function () {
-      if (this.picture.hasImage()) {
-        this.pictureURL = this.picture.generateDataUrl()
+      if (this.newUser.picture.hasImage()) {
+        this.newUser.pictureURL = this.newUser.picture.generateDataUrl()
         this.toggleImageDelete = true
       }
     },
     removeImage: function (index) {
       this.toggleImageDelete = false
-      this.picture.remove()
+      this.newUser.picture.remove()
     },
     migrateCart: function () {
       let errors = 0
@@ -262,6 +265,7 @@ export default {
     ...mapState(['guestCart'])
   },
   created () {
+    this.newUser.email = this.email
   }
 }
 </script>
