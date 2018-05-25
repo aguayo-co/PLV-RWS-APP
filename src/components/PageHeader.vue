6c4abf5dc9f0bdb419e1f711f848db0104620d38
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-05-25 14:53:53 -0500

    [Vue] Elimina referencias a API estático

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -107,7 +107,6 @@
 </template>
 
 <script>
-// import axios from 'axios'
 import PageHeaderBrand from '@/components/PageHeader-brand'
 import PageHeaderMenu from '@/components/PageHeader-menu'
 import PageHeaderSearch from '@/components/PageHeader-search'
