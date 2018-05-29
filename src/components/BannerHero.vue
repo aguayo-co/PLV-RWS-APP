8bd16cc1235f7e945b613e608987ecc5d055672f
Geraldine Diaz <geraldine.diaz@aguayo.co>
2018-05-29 17:48:10 -0500

    Front-end: Prilov-app]: Fix #177 [PRODUCT-GRID] Nombre de categoría se corta

--- src/components/BannerHero.vue
+++ src/components/BannerHero.vue
@@ -6,9 +6,17 @@
       h4.banner-hero__subtitle {{ banner.subtitle }}
     .banner-hero__main
       a.banner-hero__btn.btn(:href="banner.url") {{ banner.button_text }}
-  picture.banner-hero__crop
-    source.banner-hero__img(media='(max-width: 640px)', :srcset='banner.image')
-    img.banner-slider__img(:src='banner.image')
+  //- .user-cover-edit
+  .banner-hero__illustration
+    //- .cover
+    .banner-hero__inner
+      //- .cover__banner
+      .banner-hero__picture
+        //- .cover__picture
+        img.banner-hero__img(:src='banner.image')
+  //- picture.banner-hero__crop
+  //-   source.banner-hero__img(media='(max-width: 640px)', :srcset='banner.image')
+  //-   img.banner-slider__img(:src='banner.image')
 </template>
 
 <script>
