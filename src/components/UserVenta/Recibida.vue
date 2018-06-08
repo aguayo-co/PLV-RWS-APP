5336cd9d65abf58e2cb997c2288b0789be4e1790
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-06-08 15:29:48 -0500

    Fixes #196 - Solo se puede calificar cuando se cmplea la orden

--- src/components/UserVenta/Recibida.vue
+++ src/components/UserVenta/Recibida.vue
@@ -3,7 +3,6 @@
   .dashboard__subtitle(v-if="mqTabletMax") Estado del producto
   .dashboard__status
     p.status.status_check.i-check Pedido recibido por la vendedora
-  Calificar(:sale="sale" v-on:refresh-sale="$emit('refresh-sale', $event)")
 </template>
 
 <script src="./js/Recibida.js"></script>
