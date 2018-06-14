6f04fc710cc9d818b79a00c74ec3c96e628fe1ff
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-06-14 17:19:15 -0500

    #fixes 141 - Envía formulario a MailChimp

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -21,22 +21,43 @@ footer.page-foot.i-heart-on
         p.foot__txt Inscríbete en el Newsletter para recibir promociones, descuentos especiales y noticias.
 
         form.form.foot__form(
-          v-on:submit='',
-          action='#',
-          method='post'
+          action="https://prilov.us9.list-manage.com/subscribe/post?u=8b6af106f12d9ec63c8570ebb&amp;id=02225c7945"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          class="validate"
+          target="_blank"
+          novalidate
         )
           .form__row
             label.form__label(
               for='subscriberEmail') Correo
             input.form__control(
-              id='subscriberEmail',
-              type='email'
+              type="email"
+              value=""
+              name="EMAIL"
+              class="required email"
+              id="mce-EMAIL"
             )
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            div(
+              style="position: absolute; left: -5000px;"
+              aria-hidden="true")
+              input(type="text"
+              name="b_8b6af106f12d9ec63c8570ebb_02225c7945"
+              tabindex="-1"
+              value="")
             router-link.link_underline(
               :to="{ name: 'terminos' }") Ver Condiciones de uso
 
           .form__row.form__row_away.form__btn
-            button.btn.btn_solid Inscribir en el Newsletter
+            button.btn.btn_solid(
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="button"
+            ) Inscribir en el Newsletter
     .foot-legal
       p.foot-legal__copy Copyright © 2017 prilov.com. Todos los derechos reservados.
 
