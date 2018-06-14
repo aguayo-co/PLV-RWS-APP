27e9eae92e85e061996ea7e129a69732b3112d41
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-06-14 09:42:14 -0500

    [Vue] Configura uso de dev-tools externos

--- config/define.env.js
+++ config/define.env.js
@@ -1,5 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-  API_URL: JSON.stringify(process.env.API_URL.replace(/\/$/g, ''))
+  API_URL: JSON.stringify(process.env.API_URL.replace(/\/$/g, '')),
+  VUE_HOST: JSON.stringify(process.env.VUE_HOST.replace(/\/$/g, '')),
+  VUE_PORT: JSON.stringify(process.env.VUE_PORT),
+  VUE_URL: JSON.stringify(process.env.VUE_HOST.replace(/\/$/g, '') + ':' + process.env.VUE_PORT)
 }
