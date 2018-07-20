b4c7fb905368f1a4c5c2347543c878fb06b965fd
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-07-20 08:45:40 -0500

    [Search] Elimina orden por ID en búsqueda

--- src/components/PageHeader-search.vue
+++ src/components/PageHeader-search.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 //- Sugerencias de búsqueda a nivel de:
     marcas, categorías, nombres de Prilovers.
-form.search(action='', method='GET', @submit.prevent="search")
+form.search(@submit.prevent="search")
   .search__row
     input.search__input#searchMain(type='search', name='search', placeholder='¿Qué buscas?', v-model="query")
     input.search__btn(type='submit', value='')
