24e5b10fefe1e998aad73fa509bb4c8bb9c2fb9b
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-08-21 16:26:36 -0500

    [MailChimp] Make mailchimp optional and configurable

--- config/define.env.js
+++ config/define.env.js
@@ -4,11 +4,13 @@ const vueHost = process.env.VUE_HOST ? process.env.VUE_HOST.replace(/\/$/g, '') 
 const vuePort = process.env.VUE_PORT || null
 const vueUrl = vueHost && vuePort ? vueHost + ':' + vuePort : null
 const gaTracking = process.env.GA_TRACKING || null
+const mcUrl = process.env.MC_URL || null
 
 module.exports = {
   NODE_ENV: JSON.stringify(process.env.NODE_ENV),
   API_URL: JSON.stringify(process.env.API_URL.replace(/\/$/g, '')),
   GA_TRACKING: JSON.stringify(gaTracking),
+  MC_URL: JSON.stringify(mcUrl),
   VUE_HOST: JSON.stringify(vueHost),
   VUE_PORT: JSON.stringify(vuePort),
   VUE_URL: JSON.stringify(vueUrl)
